700732f66f21c3fb2d2dbd010a790685280ab146
Simon Larsén <slarse@kth.se>
2018-06-19 09:30:47 +0200

    Add git command wrappers (PR #5)
    * Add skeletson for git wrapper functions
    
    * Add some progress
    
    * Add git clone, fix #1 
    
    * Add git push, fix #3 
    
    * Add add push remotes, fix #2 
    
    * Fix tests failing when there is GITS_PET_OAUTH is not actually set

---
+++ gits_pet/git.py
@@ -0,0 +1,188 @@
+"""Wrapper functions for git commands.
+
+This module contains wrapper functions for git commands, such as push and clone.
+"""
+import os
+import sys
+import subprocess
+import collections
+from typing import Sequence, Tuple
+
+
+class GitError(RuntimeError):
+    """A generic error to raise when a git command exits with a non-zero
+    exit status.
+    """
+
+    def __init__(self, command: Sequence[str], returncode: int, stderr: bytes):
+        msg = ("git exited with a non-zero exit status.{}"
+               "issued command: {}{}"
+               "return code: {}{}"
+               "stderr: {}").format(
+                   os.linesep,
+                   " ".join(command),
+                   os.linesep,
+                   returncode,
+                   os.linesep,
+                   stderr.decode(encoding=sys.getdefaultencoding()))
+        super().__init__(msg)
+
+
+class CloneFailedError(GitError):
+    """An error to raise when cloning a repository fails."""
+
+
+class PushFailedError(GitError):
+    """An error to raise when pushing to a remote fails."""
+
+
+OAUTH_TOKEN = os.getenv('GITS_PET_OAUTH')
+if not OAUTH_TOKEN:
+    raise OSError('The oauth token is empty!')
+
+
+def _insert_token(https_url: str, token: str = OAUTH_TOKEN) -> str:
+    """Insert an oauth token into the https url as described here:
+        https://blog.github.com/2012-09-21-easier-builds-and-deployments-using-git-over-https-and-oauth/
+
+    Args:
+        https_url: A url on the form `https://host.topdomain`
+        token: A GitHub OAUTH token.
+
+    Returns:
+        The provided url with the token inserted
+    """
+    if not https_url.startswith('https://'):
+        raise ValueError(
+            'invalid url `{}`, does not start with `https://`'.format(
+                https_url))
+    if not token:
+        raise ValueError('invalid token, empty token not allowed')
+    return https_url.replace('https://', 'https://{}{}'.format(token, '@'))
+
+
+def quiet_run(*args, **kwargs):
+    """Run a subprocess and pipe output to /dev/null."""
+    with open(os.devnull, 'w') as devnull:
+        return subprocess.run(
+            *args, **kwargs, stdout=devnull, stderr=subprocess.STDOUT)
+
+
+def captured_run(*args, **kwargs):
+    """Run a subprocess and capture the output."""
+    proc = subprocess.run(
+        *args, **kwargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return proc.returncode, proc.stdout, proc.stderr
+
+
+def clone(repo_url: str, single_branch: bool = True, branch: str = None):
+    """Clone a git repository.
+
+    Args:
+        repo_url: HTTPS url to repository on the form https://<host>/<owner>/<repo>.
+        single_branch: Whether or not to clone a single branch.
+        branch: The branch to clone.
+    """
+    if not isinstance(repo_url, str):
+        raise TypeError(
+            'repo_url is of type {.__class__.__name__}, expected str'.format(
+                repo_url))
+    if not isinstance(single_branch, bool):
+        raise TypeError(
+            'single_branch is of type {.__class__.__name__}, expected bool'.
+            format(single_branch))
+    if not isinstance(branch, (type(None), str)):
+        raise TypeError(
+            'branch is of type {.__class__.__name__}, expected NoneType or str'
+        )
+
+    if isinstance(branch, str) and not branch:
+        raise ValueError("branch must not be empty")
+
+    options = []
+    if single_branch:
+        options.append('--single-branch')
+    if branch is not None:
+        options += ['-b', branch]
+
+    clone_command = [
+        'git', 'clone',
+        _insert_token(repo_url, OAUTH_TOKEN), *options
+    ]
+    rc, _, stderr = captured_run(clone_command)
+
+    if rc != 0:
+        raise CloneFailedError(clone_command, rc, stderr)
+
+
+def push(repo_path: str, remote: str = 'origin', branch: str = 'master'):
+    """Push a repository. 
+
+    Args:
+        repo_path: Path to the root of a git repository.
+        remote: Name of the remote to push to.
+        branch: Name of the branch to push to.
+    """
+    if not isinstance(repo_path, str):
+        raise TypeError(
+            'repo_path is of type {.__class__.__name__}, expected str'.format(
+                repo_path))
+    if not isinstance(remote, str):
+        raise TypeError(
+            'remote is of type {.__class__.__name__}, expected str'.format(
+                remote))
+    if not isinstance(branch, str):
+        raise TypeError(
+            'branch is of type {.__class__.__name__}, expected str'.format(
+                branch))
+
+    if not repo_path:
+        raise ValueError("repo_path must not be empty")
+    if not remote:
+        raise ValueError("remote must not be empty")
+    if not branch:
+        raise ValueError("branch must not be empty")
+
+    push_command = ['git', 'push', remote, branch]
+    rc, _, stderr = captured_run(push_command, cwd=os.path.abspath(repo_path))
+
+    if rc != 0:
+        raise PushFailedError(push_command, rc, stderr)
+
+
+def add_push_remotes(repo_path: str, remotes: Sequence[Sequence[str]]):
+    """Add push remotes to a repository.
+
+    Args:
+        repo_path: Path to the root of a git repository.
+        remotes: A list of (remote, repo_url) pairs to add as push remotes.
+    """
+    if not isinstance(repo_path, str):
+        raise TypeError(
+            "repo_path is of type {.__class__.__name__}, expected str".format(
+                repo_path))
+    if not isinstance(remotes, collections.Sequence):
+        raise TypeError(
+            "remotes is of type {.__class__.__name__}, expected sequence"
+            .format(remotes))
+
+    if not repo_path:
+        raise ValueError("repo_path must not be empty")
+    bad_pairs = [
+        pair for pair in remotes
+        if not isinstance(pair, collections.Sequence) or len(pair) != 2
+        or not isinstance(pair[0], str) or not isinstance(pair[1], str)
+    ]
+    if bad_pairs:
+        raise ValueError("remotes poorly formed, first bad value: {}".format(
+            str(bad_pairs[0])))
+    if not remotes:
+        raise ValueError("remotes must not be empty")
+
+    for remote, url in remotes:
+        add_remote_command = 'git remote set-url --add --push {} {}'.format(
+            remote, url).split()
+        rc, _, stderr = captured_run(
+            add_remote_command, cwd=os.path.abspath(repo_path))
+        if rc != 0:
+            raise GitError(add_remote_command, rc, stderr)
---
+++ tests/test_git.py
@@ -0,0 +1,234 @@
+import sys
+import os
+import pytest
+from unittest.mock import patch
+from collections import namedtuple
+
+URL_TEMPLATE = 'https://{}github.com/slarse/clanim'
+TOKEN = 'besttoken1337'
+
+Env = namedtuple('Env', ['expected_url'])
+
+# import with mocked oauth
+with patch('os.getenv', autospec=True, return_value=TOKEN):
+    from gits_pet import git
+
+
+def test_insert_token():
+    token = '1209487fbfuq324yfqf78b6'
+    assert git._insert_token(URL_TEMPLATE.format(''),
+                             token) == URL_TEMPLATE.format(token + '@')
+
+
+def test_insert_empty_token_raises():
+    with pytest.raises(ValueError) as exc:
+        git._insert_token(URL_TEMPLATE.format(''), '')
+    assert 'empty token' in str(exc)
+
+
+def test_insert_token_raises_for_non_https():
+    with pytest.raises(ValueError) as exc:
+        git._insert_token('www.google.com', '124124vfgh23')
+    assert 'https://' in str(exc)
+
+
+def test_clone_raises_on_type_errors(env_setup):
+    with pytest.raises(TypeError) as exc:
+        git.clone(32)
+    assert 'repo_url is of type' in str(exc)
+    assert 'expected str' in str(exc)
+
+    with pytest.raises(TypeError) as exc:
+        git.clone('blabla', 8)
+    assert 'single_branch is of type' in str(exc)
+    assert 'expected bool'
+
+    with pytest.raises(TypeError) as exc:
+        git.clone('blabla', True, 32)
+    assert 'branch is of type' in str(exc)
+    assert 'expected NoneType or str'
+
+
+def test_clone_raises_on_empty_branch(env_setup):
+    with pytest.raises(ValueError) as exc:
+        git.clone(URL_TEMPLATE.format(''), branch='')
+    assert 'branch must not be empty' in str(exc)
+
+
+def test_clone_raises_on_non_zero_exit_from_git_clone(env_setup, mocker):
+    stderr = b'This is pretty bad!'
+    # already patched in env_setup fixture
+    git.captured_run.return_value = (1, b'', stderr)
+
+    with pytest.raises(git.CloneFailedError) as exc:
+        git.clone("{}".format(URL_TEMPLATE.format('')))
+    assert stderr.decode(encoding=sys.getdefaultencoding()) in str(exc.value)
+
+
+@pytest.fixture(scope='function')
+def env_setup(mocker):
+    mocker.patch(
+        'gits_pet.git.captured_run', autospec=True, return_value=(0, b'', b''))
+    # TOKEN was mocked as the environment token when gits_pet.git was imported
+    expected_url = URL_TEMPLATE.format(TOKEN + '@')
+    return Env(expected_url=expected_url)
+
+
+def test_clone_issues_correct_command_with_defaults(env_setup):
+    expected_command = "git clone {} --single-branch".format(
+        env_setup.expected_url).split()
+
+    git.clone(URL_TEMPLATE.format(''))
+    git.captured_run.assert_called_once_with(expected_command)
+
+
+def test_clone_issues_correct_command_without_single_branch(env_setup):
+    expected_command = "git clone {}".format(env_setup.expected_url).split()
+
+    git.clone(URL_TEMPLATE.format(''), single_branch=False)
+    git.captured_run.assert_called_once_with(expected_command)
+
+
+def test_clone_issues_correct_command_with_single_other_branch(env_setup):
+    branch = 'other-branch'
+    expected_command = "git clone {} --single-branch -b {}".format(
+        env_setup.expected_url, branch).split()
+
+    git.clone(URL_TEMPLATE.format(''), single_branch=True, branch=branch)
+    git.captured_run.assert_called_once_with(expected_command)
+
+
+def test_push_raises_on_non_string_args(env_setup):
+    with pytest.raises(TypeError) as exc:
+        git.push(1)
+    assert 'repo_path' in str(exc)
+    assert 'expected str' in str(exc)
+
+    with pytest.raises(TypeError) as exc:
+        git.push('something', remote=452)
+    assert 'remote' in str(exc)
+    assert 'expected str' in str(exc)
+
+    with pytest.raises(TypeError) as exc:
+        git.push('something', remote='origin', branch=123)
+    assert 'branch' in str(exc)
+    assert 'expected str' in str(exc)
+
+
+def test_push_raises_on_empty_repo_path(env_setup):
+    with pytest.raises(ValueError) as exc:
+        git.push('')
+    assert 'repo_path must not be empty' in str(exc)
+
+
+def test_push_raises_on_empty_remote(env_setup):
+    with pytest.raises(ValueError) as exc:
+        git.push('something', remote='')
+    assert 'remote must not be empty' in str(exc)
+
+
+def test_push_raises_on_empty_branch(env_setup):
+    with pytest.raises(ValueError) as exc:
+        git.push('something', remote='origin', branch='')
+    assert 'branch must not be empty' in str(exc)
+
+
+def test_push_issues_correct_command_with_defaults(env_setup):
+    expected_command = "git push origin master".split()
+    repo_path = os.sep.join(['path', 'to', 'repo'])
+
+    git.push(repo_path)
+
+    git.captured_run.assert_called_once_with(
+        expected_command, cwd=os.path.abspath(repo_path))
+
+
+def test_push_issues_correct_command(env_setup):
+    remote = 'other'
+    branch = 'development-branch'
+    expected_command = "git push {} {}".format(remote, branch).split()
+    repo_path = os.sep.join(['path', 'to', 'repo'])
+
+    git.push(repo_path, remote=remote, branch=branch)
+
+    git.captured_run.assert_called_once_with(
+        expected_command, cwd=os.path.abspath(repo_path))
+
+
+def test_push_raises_on_non_zero_exit_from_git_clone(env_setup, mocker):
+    stderr = b'This is pretty bad indeed!'
+    # already patched in env_setup fixture
+    git.captured_run.return_value = (1, b'', stderr)
+
+    with pytest.raises(git.PushFailedError) as exc:
+        git.push('some_repo')
+    assert stderr.decode(encoding=sys.getdefaultencoding()) in str(exc.value)
+
+
+def test_add_push_remotes_raises_on_non_str_repo_path(env_setup):
+    with pytest.raises(TypeError) as exc:
+        git.add_push_remotes(2, tuple())
+    assert 'repo_path' in str(exc)
+    assert 'expected str' in str(exc)
+
+
+def test_add_push_remotes_raises_on_empty_repo_path(env_setup):
+    with pytest.raises(ValueError) as exc:
+        git.add_push_remotes('',
+                             [('origin', 'https://github.com/slarse/clanim')])
+    assert 'repo_path must not be empty' in str(exc)
+
+
+def test_add_push_remotes_raises_on_empty_remotes(env_setup):
+    with pytest.raises(ValueError) as exc:
+        git.add_push_remotes('something', [])
+    assert 'remotes' in str(exc)
+
+
+def test_add_push_remotes_raises_on_bad_remotes_formatting(env_setup):
+    repo = 'something'
+    # second tuple has too many values
+    remotes_bad_length = (('origin', 'https://byebye.com'),
+                          ('origin', 'https://hello.com', 'byeby'))
+
+    # third tuple has an int as remote
+    remotes_bad_type = (('origin', 'https://byebye.com'),
+                        ('origin', 'https://hello.com'), (2,
+                                                          'https://slarse.se'))
+    with pytest.raises(ValueError) as exc_bad_length:
+        git.add_push_remotes(repo, remotes_bad_length)
+
+    with pytest.raises(ValueError) as exc_bad_type:
+        git.add_push_remotes(repo, remotes_bad_type)
+
+    assert str(remotes_bad_length[1]) in str(exc_bad_length)
+    assert str(remotes_bad_type[2]) in str(exc_bad_type)
+
+
+def test_add_push_remotes(env_setup):
+    repo = os.sep.join(['some', 'repo', 'path'])
+    remotes = (('origin', 'https://slarse.se/repo'),
+               ('origin', 'https://github.com/slarse/repo'),
+               ('other', 'https://github.com/slarse/repo'))
+
+    expected_commands = [
+        "git remote set-url --add --push {} {}".format(remote, url).split()
+        for remote, url in remotes
+    ]
+    git.add_push_remotes(repo, remotes)
+
+    for command in expected_commands:
+        git.captured_run.assert_any_call(command, cwd=os.path.abspath(repo))
+
+
+def test_add_push_remotes_raises_on_non_zero_exit_from_git(env_setup):
+    stderr = b'bad for push remotes'
+    git.captured_run.return_value = (23, b'', stderr)
+    repo = os.sep.join(['some', 'repo', 'path'])
+    remotes = (('origin', 'https://slarse.se/repo'),
+               ('origin', 'https://github.com/slarse/repo'),
+               ('other', 'https://github.com/slarse/repo'))
+
+    with pytest.raises(git.GitError) as exc:
+        git.add_push_remotes(repo, remotes)
+    assert stderr.decode(encoding=sys.getdefaultencoding()) in str(exc.value)
