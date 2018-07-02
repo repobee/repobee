9d1b750af1d37004c0bf47bfefde8d850a7e16fc
Simon Larsén <slarse@kth.se>
2018-07-02 11:39:31 +0200

    Remove pygithub as a test requirement

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 """Test setup."""
-from unittest.mock import patch
+import sys
+from unittest.mock import patch, MagicMock
 
 # the git module must be imported with a mocked env variable
 TOKEN = 'besttoken1337'
 with patch('os.getenv', autospec=True, return_value=TOKEN):
     import gits_pet
     from gits_pet import git
+
+# mock the PyGithub github module
+sys.modules['github'] = MagicMock()
+
 
 assert TOKEN == gits_pet.git.OAUTH_TOKEN
