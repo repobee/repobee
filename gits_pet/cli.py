repobee/repobee

commit b7ea93564aedca442b02e426a847d2559f718195

Add graceful exit when no student repos are found, fix #37

gits_pet/cli.py
@@ -54,6 +54,10 @@ CONFIGURABLE_ARGS = set(('user', 'org_name', 'github_base_url',
                          'students_file'))
 
 
+class ParseError(Exception):
+    """Raise when something goes wrong in parsing."""
+
+
 class FileError(IOError):
     """Raise when reading or writing to a file errors out."""
 
@@ -79,7 +83,8 @@ def parse_args(sys_args: Iterable[str]) -> (tuples.Args, github_api.GitHubAPI):
         master_urls = args.master_repo_urls
         master_names = [util.repo_name(url) for url in master_urls]
     elif 'master_repo_names' in args:
-        master_urls = api.get_repo_urls(args.master_repo_names)
+        master_urls, not_found = api.get_repo_urls(args.master_repo_names)
+        # TODO possibly raise ParseError if not_found is not empty
         master_names = args.master_repo_names
     else:
         master_urls = None
@@ -110,32 +115,32 @@ def handle_parsed_args(args: tuples.Args, api: github_api.GitHubAPI):
         api: An initialized GitHubAPI instance.
     """
     if args.subparser == ADD_TO_TEAMS_PARSER:
-        with _sys_exit_on_git_error():
+        with _sys_exit_on_expected_error():
             admin.add_students_to_teams(args.students, api)
     elif args.subparser == SETUP_PARSER:
-        with _sys_exit_on_git_error():
+        with _sys_exit_on_expected_error():
             admin.setup_student_repos(
                 master_repo_urls=args.master_repo_urls,
                 students=args.students,
                 user=args.user,
                 api=api)
     elif args.subparser == UPDATE_PARSER:
-        with _sys_exit_on_git_error():
+        with _sys_exit_on_expected_error():
             admin.update_student_repos(args.master_repo_urls, args.students,
                                        args.user, api)
     elif args.subparser == OPEN_ISSUE_PARSER:
-        with _sys_exit_on_git_error():
+        with _sys_exit_on_expected_error():
             admin.open_issue(args.master_repo_names, args.students, args.issue,
                              api)
     elif args.subparser == CLOSE_ISSUE_PARSER:
-        with _sys_exit_on_git_error():
+        with _sys_exit_on_expected_error():
             admin.close_issue(args.title_regex, args.master_repo_names,
                               args.students, api)
     elif args.subparser == MIGRATE_PARSER:
-        with _sys_exit_on_git_error():
+        with _sys_exit_on_expected_error():
             admin.migrate_repos(args.master_repo_urls, args.user, api)
     elif args.subparser == CLONE_PARSER:
-        with _sys_exit_on_git_error():
+        with _sys_exit_on_expected_error():
             admin.clone_repos(args.master_repo_names, args.students, api)
     else:
         raise ValueError("Illegal value for subparser: {}".format(
@@ -362,7 +367,8 @@ def _get_configured_defaults(config_file=DEFAULT_CONFIG_FILE):
 
 
 @contextmanager
-def _sys_exit_on_git_error():
+def _sys_exit_on_expected_error():
+    """Expect either git.GitError or github_api.APIError."""
     try:
         yield
     except git.PushFailedError as exc:
@@ -377,6 +383,9 @@ def _sys_exit_on_git_error():
         sys.exit(1)
     except git.GitError as exc:
         LOGGER.error("Something went wrong with git. See the logs for info.")
+        sys.exit(1)
+    except github_api.APIError as exc:
+        LOGGER.error("Exiting beacuse of {.__class__.__name__}".format(exc))
         sys.exit(1)
 
 

gits_pet/admin.py
@@ -15,6 +15,7 @@ from typing import Iterable, List, Optional
 from collections import namedtuple
 import daiquiri
 from gits_pet import git
+from gits_pet import github_api
 from gits_pet import util
 from gits_pet import tuples
 from gits_pet.github_api import GitHubAPI, RepoInfo
@@ -150,18 +151,25 @@ def update_student_repos(master_repo_urls: Iterable[str],
         master_repo_urls=master_repo_urls, user=user, students=students)
     urls = list(master_repo_urls)  # safe copy
 
-    LOGGER.info("cloning into master repos ...")
-    _clone_all(urls)
+    if len(set(urls)) != len(urls):
+        raise ValueError("master_repo_urls contains duplicates")
 
     master_repo_names = [util.repo_name(url) for url in urls]
-    student_repo_names = [
-        util.generate_repo_name(student, master_repo_name)
-        for student in students for master_repo_name in master_repo_names
-    ]
+    student_repo_names = util.generate_repo_names(students, master_repo_names)
 
-    repo_urls = api.get_repo_urls(student_repo_names)
+    repo_urls, not_found = api.get_repo_urls(student_repo_names)
+
+    if not repo_urls:
+        msg = "No student repos corresponding to the master repos were found"
+        LOGGER.error(msg)
+        raise github_api.APIError(msg)
+    elif not_found:
+        LOGGER.warning("Ignoring repos that were not found")
 
     push_tuples = _create_push_tuples(urls, repo_urls)
+
+    LOGGER.info("cloning into master repos ...")
+    _clone_all(urls)
 
     LOGGER.info("pushing files to student repos ...")
     failed_urls = git.push(push_tuples, user=user)
@@ -200,10 +208,7 @@ def open_issue(issue: tuples.Issue, master_repo_names: Iterable[str],
     util.validate_non_empty(
         master_repo_names=master_repo_names, students=students, issue=issue)
 
-    repo_names = [
-        util.generate_repo_name(student, master)
-        for master in master_repo_names for student in students
-    ]
+    repo_names = util.generate_repo_names(students, master_repo_names)
 
     api.open_issue(issue.title, issue.body, repo_names)
 
@@ -224,10 +229,7 @@ def close_issue(title_regex: str, master_repo_names: Iterable[str],
         master_repo_names=master_repo_names,
         students=students)
 
-    repo_names = [
-        util.generate_repo_name(student, master)
-        for master in master_repo_names for student in students
-    ]
+    repo_names = util.generate_repo_names(students, master_repo_names)
 
     api.close_issue(title_regex, repo_names)
 
@@ -245,11 +247,8 @@ def clone_repos(master_repo_names: Iterable[str], students: Iterable[str],
     util.validate_non_empty(
         master_repo_names=master_repo_names, students=students)
 
-    repo_names = [
-        util.generate_repo_name(student, master_name) for student in students
-        for master_name in master_repo_names
-    ]
-    repo_urls = api.get_repo_urls(repo_names)
+    repo_names = util.generate_repo_names(students, master_repo_names)
+    repo_urls, _ = api.get_repo_urls(repo_names)
 
     LOGGER.info("cloning into student repos ...")
     git.clone(repo_urls)

gits_pet/github_api.py
@@ -12,9 +12,12 @@ import github
 from gits_pet.api_wrapper import (ApiWrapper, RepoInfo, GitHubError,
                                   UnexpectedException, Team, _Team, _User,
                                   _Repo)
+from gits_pet import util
 
 LOGGER = daiquiri.getLogger(__file__)
 
+class APIError(Exception):
+    """Raise when something unexpected happens when interacting with the API."""
 
 class GitHubAPI:
     """A highly specialized GitHub API class for gits_pet."""
@@ -159,20 +162,30 @@ class GitHubAPI:
                     "An unexpected exception was raised.")
         return repo_urls
 
-    def get_repo_urls(self, repo_names: Iterable[str]) -> List[str]:
+    def get_repo_urls(self,
+                      repo_names: Iterable[str]) -> (List[str], List[str]):
         """Get repo urls for all specified repo names in the current organization.
 
         Args:
             repo_names: A list of repository names.
 
         Returns:
-            a list of urls corresponding to the repo names.
+            a list of urls corresponding to the repo names, and a list of repo names
+            that were not found.
         """
         repo_names_set = set(repo_names)
-        return [
+        urls = [
             repo.html_url for repo in self._api.get_repos()
             if repo.name in repo_names_set
         ]
+
+        not_found = list(repo_names_set -
+                         {util.repo_name(url)
+                          for url in urls})
+        if not_found:
+            for repo_name in not_found:
+                LOGGER.warning("repo {} could not be found".format(repo_name))
+        return urls, not_found
 
     def get_repo_urls_by_regex(self, regex: str) -> List[str]:
         """Get repo urls for all repos in the current organization whose names

gits_pet/util.py
@@ -1,6 +1,7 @@
 """Some general utility functions."""
 import os
 import sys
+from typing import Iterable
 from gits_pet import tuples
 
 
@@ -62,7 +63,29 @@ def generate_repo_name(team_name: str, master_repo_name: str) -> str:
     return "{}-{}".format(team_name, master_repo_name)
 
 
-def repo_name(repo_url):
+def generate_repo_names(team_names: Iterable[str],
+                        master_repo_names: Iterable[str]) -> Iterable[str]:
+    """Construct all combinations of generate_repo_name(team_name, master_repo_name) for the provided
+    team names and master repo names.
+
+    Args:
+        team_names: One or more names of teams.
+        master_repo_names: One or more names of master repositories.
+
+    Returns:
+        a list of repo names for all combinations of team and master repo.
+    """
+    validate_non_empty(
+        team_names=team_names, master_repo_names=master_repo_names)
+    master_repo_names = list(
+        master_repo_names)  # needs to be traversed multiple times
+    return [
+        generate_repo_name(team_name, master_name)
+        for master_name in master_repo_names for team_name in team_names
+    ]
+
+
+def repo_name(repo_url: str) -> str:
     """Extract the name of the repo from its url.
 
     Args:

tests/test_admin.py
@@ -23,9 +23,9 @@ API = github_api.GitHubAPI("bla", "bla", "bla")
 ISSUE = tuples.Issue("Oops, something went wrong!",
                      "This is the body **with some formatting**.")
 
-GENERATE_TEAM_REPO_URL = lambda base_name, student:\
+GENERATE_TEAM_REPO_URL = lambda student, base_name:\
         "https://slarse.se/repos/{}".format(
-            util.generate_repo_name(base_name, student))
+            util.generate_repo_name(student, base_name))
 
 GENERATE_REPO_URL = lambda name: GENERATE_TEAM_REPO_URL(name, 'd')[:-2]
 
@@ -60,7 +60,10 @@ def git_mock(request, mocker):
     """
     if 'nogitmock' in request.keywords:
         return
-    return mocker.patch('gits_pet.admin.git', autospec=True)
+    pt = gits_pet.git.Push
+    git_mock = mocker.patch('gits_pet.admin.git', autospec=True)
+    git_mock.Push = pt
+    return git_mock
 
 
 @pytest.fixture(autouse=True)
@@ -70,6 +73,10 @@ def api_mock(request, mocker):
     mock = MagicMock(spec=gits_pet.admin.GitHubAPI)
     api_class = mocker.patch('gits_pet.admin.GitHubAPI', autospec=True)
     api_class.return_value = mock
+
+    url_from_repo_info = lambda repo_info: GENERATE_REPO_URL(repo_info.name)
+    mock.create_repos.side_effect =\
+        lambda repo_infos: list(map(url_from_repo_info, repo_infos))
     return mock
 
 
@@ -115,19 +122,15 @@ def repo_infos(master_urls, students):
 
 @pytest.fixture(scope='function')
 def push_tuples(master_urls, students):
-    push_tuples = []
-    base_names = list(map(util.repo_name, master_urls))
-    repo_urls = [
-        GENERATE_TEAM_REPO_URL(base_name, student) for student in students
-        for base_name in base_names
+
+    push_tuples = [
+        git.Push(
+            local_path=util.repo_name(url),
+            repo_url=GENERATE_TEAM_REPO_URL(student, util.repo_name(url)),
+            branch='master')
+        # note that the order here is significant, must correspond with util.generate_repo_names
+        for url in master_urls for student in students
     ]
-    for url in master_urls:
-        repo_base_name = util.repo_name(url)
-        push_tuples += [
-            git.Push(
-                local_path=repo_base_name, repo_url=repo_url, branch='master')
-            for repo_url in repo_urls if repo_url.endswith(repo_base_name)
-        ]
     return push_tuples
 
 
@@ -216,15 +219,21 @@ class TestSetupStudentRepos:
                         git_mock, repo_infos, push_tuples, rmtree_mock,
                         ensure_teams_and_members_mock):
         """Test that setup_student_repos makes the correct function calls."""
+        expected_clone_calls = [call(url) for url in master_urls]
+        expected_rmtree_calls = [
+            call(util.repo_name(url)) for url in master_urls
+        ]
+        expected_ensure_teams_arg = {
+            student: [student]
+            for student in students
+        }
+
         admin.setup_student_repos(master_urls, students, USER, api_mock)
 
-        for url in master_urls:
-            git_mock.clone_single.assert_any_call(url)
-            api_mock.ensure_teams_and_members.assert_called_once_with(
-                {student: [student]
-                 for student in students})
-            rmtree_mock.assert_any_call(util.repo_name(url))
-
+        git_mock.clone_single.assert_has_calls(expected_clone_calls)
+        rmtree_mock.assert_has_calls(expected_rmtree_calls)
+        api_mock.ensure_teams_and_members.assert_called_once_with(
+            expected_ensure_teams_arg)
         api_mock.create_repos.assert_called_once_with(repo_infos)
         git_mock.push.assert_called_once_with(push_tuples, user=USER)
 
@@ -268,15 +277,24 @@ class TestUpdateStudentRepos:
             admin.update_student_repos(master_urls, students, user, api)
         assert type_error_arg in str(exc_info.value)
 
-    def test_happy_path(self, mocker, master_urls, students, api_mock,
-                        git_mock, push_tuples, rmtree_mock):
-        """Test that update_student_repos makes the correct function calls."""
+    def test_happy_path(self, git_mock, master_urls, students, api_mock,
+                        push_tuples, rmtree_mock):
+        """Test that update_student_repos makes the correct function calls.
+        
+        NOTE: Ignores the git mock.
+        """
+        expected_clone_calls = [call(url) for url in master_urls]
+        expected_rmtree_calls = [
+            call(util.repo_name(url)) for url in master_urls
+        ]
+
+        api_mock.get_repo_urls.side_effect = lambda repo_names: \
+            (list(map(GENERATE_REPO_URL, repo_names)), [])
+
         admin.update_student_repos(master_urls, students, USER, api_mock)
 
-        for url in master_urls:
-            git_mock.clone_single.assert_any_call(url)
-            rmtree_mock.assert_any_call(util.repo_name(url))
-
+        git_mock.clone_single.assert_has_calls(expected_clone_calls)
+        rmtree_mock.assert_has_calls(expected_rmtree_calls)
         git_mock.push.assert_called_once_with(push_tuples, user=USER)
 
     @pytest.mark.nogitmock
@@ -303,7 +321,7 @@ class TestUpdateStudentRepos:
         ]
         fail_repo_urls = [generate_url(name) for name in fail_repo_names]
 
-        api_mock.get_repo_urls.side_effect = lambda repo_names: [generate_url(name) for name in repo_names]
+        api_mock.get_repo_urls.side_effect = lambda repo_names: ([generate_url(name) for name in repo_names], [])
 
         async def raise_specific(pt, user):
             if pt.repo_url in fail_repo_urls:
@@ -344,7 +362,7 @@ class TestUpdateStudentRepos:
         ]
         fail_repo_urls = [generate_url(name) for name in fail_repo_names]
 
-        api_mock.get_repo_urls.side_effect = lambda repo_names: [generate_url(name) for name in repo_names]
+        api_mock.get_repo_urls.side_effect = lambda repo_names: ([generate_url(name) for name in repo_names], [])
         issue = tuples.Issue("Oops", "Sorry, we failed to push to your repo!")
 
         async def raise_specific(pt, branch):
@@ -452,10 +470,10 @@ class TestCloneRepos:
     def test_happy_path(self, api_mock, git_mock, master_names, students):
         """Tests that the correct calls are made when there are no errors."""
         expected_urls = [
-            GENERATE_TEAM_REPO_URL(student, name) for student in students
-            for name in master_names
+            GENERATE_REPO_URL(name)
+            for name in util.generate_repo_names(students, master_names)
         ]
-        api_mock.get_repo_urls.side_effect = lambda repo_names: list(map(GENERATE_REPO_URL, repo_names))
+        api_mock.get_repo_urls.side_effect = lambda repo_names: (list(map(GENERATE_REPO_URL, repo_names)), [])
 
         admin.clone_repos(master_names, students, api_mock)
 

commit e320226d3abc461d84f8b29fab2b37832d6d0bb2

Fix CLI bug introduced by b7ea935

gits_pet/cli.py
@@ -83,7 +83,9 @@ def parse_args(sys_args: Iterable[str]) -> (tuples.Args, github_api.GitHubAPI):
         master_urls = args.master_repo_urls
         master_names = [util.repo_name(url) for url in master_urls]
     elif 'master_repo_names' in args:
+        print(args.master_repo_names)
         master_urls, not_found = api.get_repo_urls(args.master_repo_names)
+        print(master_urls)
         # TODO possibly raise ParseError if not_found is not empty
         master_names = args.master_repo_names
     else:

tests/test_cli.py
@@ -31,7 +31,7 @@ COMPLETE_PUSH_ARGS = [*BASE_ARGS, *BASE_PUSH_ARGS]
 @pytest.fixture(autouse=True)
 def api_instance_mock(mocker):
     instance_mock = MagicMock(spec=gits_pet.github_api.GitHubAPI)
-    instance_mock.get_repo_urls.side_effect = lambda repo_names: list(map(GENERATE_REPO_URL, repo_names))
+    instance_mock.get_repo_urls.side_effect = lambda repo_names: (list(map(GENERATE_REPO_URL, repo_names)), [])
     return instance_mock
 
 