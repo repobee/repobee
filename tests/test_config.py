d603444ab9749525f3f2fa4c62e22d5cb3e3fc12
Simon Larsén <slarse@kth.se>
2018-07-21 11:34:40 +0200

    Full coverage of config module

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from gits_pet import config
 from gits_pet import exception
@@ -21,9 +22,45 @@ class TestGetConfiguredDefaults:
         assert "does not contain the required [DEFAULTS] header" in str(
             exc_info)
 
-    def test_get_configured_defaults_reads_full_config(self, config_mock, students_file):
+    def test_get_configured_defaults_reads_full_config(self, config_mock,
+                                                       students_file):
         defaults = config.get_configured_defaults()
         assert defaults['user'] == USER
         assert defaults['github_base_url'] == GITHUB_BASE_URL
         assert defaults['org_name'] == ORG_NAME
         assert defaults['students_file'] == str(students_file)
+
+    def test_get_configured_defaults_raises_on_invalid_keys(
+            self, empty_config_mock, students_file):
+        invalid_key = "not_valid_key"
+        config_contents = os.linesep.join([
+            "[DEFAULTS]",
+            "github_base_url = {}".format(GITHUB_BASE_URL),
+            "user = {}".format(USER),
+            "org_name = {}".format(ORG_NAME),
+            "students_file = {!s}".format(students_file),
+            "{} = whatever".format(invalid_key),
+        ])
+        empty_config_mock.write(config_contents)
+
+        with pytest.raises(exception.FileError) as exc_info:
+            config.get_configured_defaults()
+
+        assert "invalid keys" in str(exc_info)
+        assert invalid_key in str(exc_info)
+
+    def test_get_configured_defaults_raises_on_missing_header(
+            self, empty_config_mock, students_file):
+        config_contents = os.linesep.join([
+            "github_base_url = {}".format(GITHUB_BASE_URL),
+            "user = {}".format(USER),
+            "org_name = {}".format(ORG_NAME),
+            "students_file = {!s}".format(students_file),
+        ])
+        empty_config_mock.write(config_contents)
+
+        with pytest.raises(exception.FileError) as exc_info:
+            config.get_configured_defaults()
+
+        assert "does not contain the required [DEFAULTS] header" in str(
+            exc_info)
