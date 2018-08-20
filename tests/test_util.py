79a9bc4618cb4e346b32a9fc718c346f4c58ce32
Simon Larsen <slarse@kth.se>
2018-08-20 15:23:06 +0200

    Add find files utility function

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import itertools
 import tempfile
 from contextlib import contextmanager
 import pytest
@@ -79,8 +80,8 @@ class TestReadIssue:
 @pytest.mark.parametrize('team_name, master_repo_name, empty_arg',
                          [('', 'some-repo-name', 'team_name'),
                           ('some-team-name', '', 'master_repo_name')])
-def test_generate_repo_name_raises_on_empty_arg(team_name,
-                                                master_repo_name, empty_arg):
+def test_generate_repo_name_raises_on_empty_arg(team_name, master_repo_name,
+                                                empty_arg):
     with pytest.raises(ValueError) as exc:
         util.generate_repo_name(team_name, master_repo_name)
     assert empty_arg in str(exc.value)
@@ -94,3 +95,96 @@ def test_generate_repo_name_raises_on_empty_arg(team_name,
      ('https://gitrepourls/repos/some-repo/base-name.git', 'base-name')])
 def test_repo_name_extracts_correct_part(url, expected_name):
     assert util.repo_name(url) == expected_name
+
+
+@pytest.fixture
+def directory_structure(tmpdir):
+    """Create a directory structure like this:
+
+
+                       root
+                    _____|___________________
+                    |    |           |       |
+                  repo Main.java  epic.py  other_repo
+            ________|________          ______|_________
+            |       |       |          |     |        |
+        README.md test.py best.py    Main.java test.py epic.py
+    """
+    root_files = ['Main.java', 'epic.py']
+    repo_files = ['README.md', 'test.py', 'best.py']
+    other_repo_files = ['Main.java', 'test.py', 'epic.py']
+
+    repo = tmpdir.mkdir("repo")
+    other_repo = tmpdir.mkdir("other_repo")
+
+    for dir_, filenames in [(tmpdir, root_files), (repo, repo_files),
+                            (other_repo, other_repo_files)]:
+        for filename in filenames:
+            file = dir_.join(filename)
+            file.ensure()
+
+    java_files = [
+        str(f)
+        for f in (tmpdir.join('Main.java'), other_repo.join('Main.java'))
+    ]
+    python_files = [
+        str(f) for f in (
+            tmpdir.join('epic.py'),
+            repo.join('test.py'),
+            repo.join('best.py'),
+            other_repo.join('test.py'),
+            other_repo.join('epic.py'),
+        )
+    ]
+    readme_files = [str(repo.join('README.md'))]
+
+    return tmpdir, java_files, python_files, readme_files
+
+
+class TestFindFilesByExtension:
+    """Tests for find_files_by_extension."""
+
+    def test_find_java_files(self, directory_structure):
+        root, java_files, *_ = directory_structure
+
+        files = list(util.find_files_by_extension(str(root), '.java'))
+
+        filepaths = [str(file.resolve()) for file in files]
+
+        assert sorted(filepaths) == sorted(java_files)
+
+    def test_find_python_files(self, directory_structure):
+        root, _, python_files, *r = directory_structure
+
+        files = list(util.find_files_by_extension(str(root), '.py'))
+
+        filepaths = [str(file.resolve()) for file in files]
+
+        assert sorted(filepaths) == sorted(python_files)
+
+    def test_find_java_py_md_files(self, directory_structure):
+        """Try to find all three types at the same time."""
+        root, java_files, python_files, md_files = directory_structure
+        expected_files = list(
+            itertools.chain(java_files, python_files, md_files))
+
+        files = list(
+            util.find_files_by_extension(str(root), '.java', '.py', '.md'))
+
+        filepaths = [str(file.resolve()) for file in files]
+
+        assert sorted(filepaths) == sorted(expected_files)
+
+    def test_works_when_no_files_found(self, directory_structure):
+        root, *_ = directory_structure
+
+        files = list(util.find_files_by_extension(str(root), '.doesnotexist'))
+
+        assert not files
+
+    def test_does_not_find_files_when_dot_is_omitted(self, directory_structure):
+        root, *_ = directory_structure
+
+        files = list(util.find_files_by_extension(str(root), 'java'))
+
+        assert not files
