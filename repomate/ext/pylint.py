e183cc2646f7ff1951ad0cf783b552476c247841
Simon Larsen <slarse@kth.se>
2018-08-21 18:04:11 +0200

    Clean up and improve docs in plugins

--- repomate/ext/pylint.py
+++ repomate/ext/pylint.py
@@ -1,29 +1,65 @@
-from repomate.hookspec import hookimpl
+"""Plugin that runs javac on all files in a repo.
+
+.. important::
+
+    Requires ``pylint`` to be installed and accessible by the script!
+
+This plugin is mostly for demonstrational purposes, showing how to make the
+most barebones of plugins using only a single function. It finds all ``.py``
+files in a repo, and runs pylint on them, storing the results in files named
+``<filename>.lint`` for any ``.py`` file named ``filename``.
+
+.. module:: pylint
+    :synopsis: Plugin that runs pylint on all .py files in a repo.
+
+.. moduleauthor:: Simon Larsén
+"""
+
 import subprocess
 import os
+import pathlib
+from typing import Tuple, Union, Iterable
+
 import daiquiri
+
+from repomate import plugin
 from repomate import tuples
+from repomate import util
+from repomate.hookspec import hookimpl
 
 LOGGER = daiquiri.getLogger(name=__file__)
 
 
-def _python_files(root):
-    for cwd, dirs, files in os.walk(root):
-        for file in files:
-            if file.endswith('.py'):
-                yield cwd, file
-
-
 @hookimpl
-def act_on_cloned_repo(path):
-    """Run pylint on all Python files in a repo."""
-    python_files = list(_python_files(path))
+def act_on_cloned_repo(path: Union[str, pathlib.Path]):
+    """Run pylint on all Python files in a repo.
+    
+    Args:
+        path: Path to the repo.
+    Returns:
+        a tuples.HookResult specifying the outcome.
+    """
+    python_files = list(util.find_files_by_extension(path, '.py'))
 
     if not python_files:
         msg = "no .py files found"
         status = "warning"
         return tuples.HookResult('pylint', status, msg)
 
+    status, msg = _pylint(python_files)
+    return tuples.HookResult(hook="pylint", status="success", msg=msg)
+
+
+def _pylint(
+        python_files: Iterable[Union[str, pathlib.Path]]) -> Tuple[str, str]:
+    """Run ``pylint`` on all of the specified files.
+
+    Args:
+        python_files: paths to ``.py`` files.
+    Returns:
+        (status, msg), where status is e.g. :py:const:`plugin.ERROR` and
+        the message describes the outcome in plain text.
+    """
     linted_files = []
     for cwd, py_file in python_files:
         LOGGER.info("running pylint on {}".format(py_file))
@@ -32,9 +68,10 @@ def act_on_cloned_repo(path):
         command = 'pylint {}'.format(infile).split()
         proc = subprocess.run(
             command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
         with open(outfile, 'w', encoding='utf8') as f:
             f.write(proc.stdout.decode('utf8'))
         linted_files.append(py_file)
 
     msg = "linted files: {}".format(", ".join(linted_files))
-    return tuples.HookResult(hook="pylint", status="success", msg=msg)
+    return plugin.SUCCESS, msg
--- repomate/hookspec.py
+++ repomate/hookspec.py
@@ -8,9 +8,11 @@
 import pathlib
 import argparse
 import configparser
-from typing import Union
+from typing import Union, Optional
 
 import pluggy
+
+from repomate import tuples
 
 hookspec = pluggy.HookspecMarker(__package__)
 hookimpl = pluggy.HookimplMarker(__package__)
@@ -22,15 +24,21 @@ class CloneHook:
     """Hook functions related to cloning repos."""
 
     @hookspec
-    def act_on_cloned_repo(self, path: Union[str, pathlib.Path]):
+    def act_on_cloned_repo(self, path: Union[str, pathlib.Path]
+                           ) -> Optional[tuples.HookResult]:
         """Do something with a cloned repo.
 
         Args:
             path: Path to the repo.
+
+        Returns:
+            optionally returns a tuples.HookResult namedtuple for reporting the
+            outcome of the hook. May also return None, in which case no
+            reporting will be performed for the hook.
         """
 
     @hookspec
-    def clone_parser_hook(self, clone_parser: argparse.ArgumentParser):
+    def clone_parser_hook(self, clone_parser: argparse.ArgumentParser) -> None:
         """Do something with the clone repos subparser before it is used used to
         parse CLI options. The typical task is to add options to it.
 
@@ -39,7 +47,7 @@ class CloneHook:
         """
 
     @hookspec
-    def parse_args(self, args: argparse.Namespace):
+    def parse_args(self, args: argparse.Namespace) -> None:
         """Get the raw args from the parser. Only called for the clone parser.
         The typical task is to fetch any values from options added in
         :py:func:`clone_parser_hook`.
@@ -50,7 +58,7 @@ class CloneHook:
         """
 
     @hookspec
-    def config_hook(self, config_parser: configparser.ConfigParser):
+    def config_hook(self, config_parser: configparser.ConfigParser) -> None:
         """Hook into the config file parsing.
         
         Args:
