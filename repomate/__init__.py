72e08317ed726a2f96b5aaad4175a5cadb96dd1e
Simon Larsen <slarse@kth.se>
2018-08-22 11:41:54 +0200

    Fix errors in setup.py, bump patch version

--- repomate/__init__.py
+++ repomate/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Simon Larsén'
 
 from repomate.pygithub_wrapper import PyGithubWrapper as APIWrapper
--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open('README.md', mode='r', encoding='utf-8') as f:
     readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
 
 test_requirements = [
     'appdirs', 'daiquiri', 'pytest>=3.1.1', 'pytest-cov>=2.5.1', 'pytest-mock',
@@ -14,30 +11,30 @@ required = ['appdirs', 'daiquiri', 'pygithub', 'colored', 'pluggy']
 
 setup(
     name='repomate',
-    version='0.1.0',
-    description=('A CLI tool for GitHub'),
+    version='0.1.1',
+    description=(
+        'A CLI tool for managing large amounts of GitHub repositories'),
     long_description=readme,
     long_description_content_type='text/markdown',
     author='Simon Larsén',
     author_email='slarse@kth.se',
     url='https://github.com/slarse/repomate',
-    download_url='https://github.com/slarse/repomate/archive/v0.1.0.tar.gz',
-    license=license,
+    download_url='https://github.com/slarse/repomate/archive/v0.1.1.tar.gz',
+    license='MIT',
     packages=find_packages(exclude=('tests', 'docs')),
     tests_require=test_requirements,
     install_requires=required,
     extras_require=dict(TEST=test_requirements),
     scripts=['bin/repomate'],
     include_package_data=True,
-    zip_safe=False,
+    zip_save=False,
     classifiers=[
-        'Development Status :: 5 - Beta',
-        'Intended Audience :: Teachers',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: Linux'
-        'Operating System :: macOS',
+        'Operating System :: POSIX',
     ])
