9b0c834735dcad982b3a3d19dd5edb56ac6de215
Simon Larsén <slarse@kth.se>
2018-09-19 13:19:44 +0200

    Add plugin cli options, fix #82 (PR #87)
    * Add fairly ugly hack for specifying plugins on cli
    
    * Add plugin commands to cli
    
    * Improve the plugin pre-parser

--- repomate/main.py
+++ repomate/main.py
@@ -7,25 +7,52 @@
 """
 
 import sys
+import itertools
 import daiquiri
+from typing import List
 
 from repomate import cli
-from repomate import exception
 from repomate import plugin
 
 LOGGER = daiquiri.getLogger(__file__)
 
 
-# if the OAUTH token is not set, OSError is raised
-def main():
+def _separate_args(args: List[str]) -> (List[str], List[str]):
+    """Separate args into plugin args and repomate args."""
+    plugin_args = []
+    if args and (args[0].startswith('-p') or 'plug' in args[0]):
+        cur = 0
+        while cur < len(args) and args[cur].startswith('-'):
+            if args[cur].startswith('-p'):
+                plugin_args += args[cur:cur + 2]
+                cur += 2
+            elif args[cur] == '--no-plugins':
+                plugin_args.append(args[cur])
+                cur += 1
+            else:
+                break
+    return plugin_args, args[len(plugin_args):]
+
+
+def main(sys_args: List[str]):
     """Start the repomate CLI."""
+    args = sys_args[1:]  # drop the name of the program
     try:
-        plugin.initialize_plugins()
-        parsed_args, api = cli.parse_args(sys.argv[1:])
+        plugin_args, app_args = _separate_args(args)
+
+        if plugin_args:
+            parsed_plugin_args = cli.parse_plugins(plugin_args)
+            if parsed_plugin_args.no_plugins:
+                LOGGER.info("plugins disabled")
+            else:
+                plugin.initialize_plugins(parsed_plugin_args.plug)
+        else:
+            plugin.initialize_plugins()
+        parsed_args, api = cli.parse_args(app_args)
         cli.dispatch_command(parsed_args, api)
     except Exception as exc:
         LOGGER.error("{.__class__.__name__}: {}".format(exc, str(exc)))
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch, MagicMock
 import builtins
 import pytest
+from collections import namedtuple
 
 from pytest.functions import raise_
 import repomate
@@ -24,17 +25,22 @@ VALID_PARSED_ARGS = dict(
 
 PARSED_ARGS = tuples.Args(cli.SETUP_PARSER, **VALID_PARSED_ARGS)
 
+CLONE_ARGS = 'clone -mn week-2 -s slarse'.split()
 
-@pytest.fixture
-def monkeypatch_sys_args(monkeypatch):
-    sys_args = ['just some made up arguments'.split()]
-    monkeypatch.setattr('sys.argv', sys_args)
-    return sys_args
+module = namedtuple('module', ('name', ))
 
 
 @pytest.fixture
 def api_instance_mock(mocker):
     return MagicMock(spec='repomate.APIWrapper')
+
+
+@pytest.fixture
+def init_plugins_mock(mocker):
+    return mocker.patch(
+        'repomate.plugin.initialize_plugins',
+        autospec=True,
+        side_effect=lambda plugs: list(map(module, plugs)))
 
 
 @pytest.fixture
@@ -46,37 +52,104 @@ def parse_args_mock(mocker, api_instance_mock):
 
 
 @pytest.fixture
+def parse_plugins_mock(mocker):
+    return mocker.patch(
+        'repomate.cli.parse_plugins',
+        autospec=True,
+        side_effect=
+        lambda args: [arg for arg in args if not arg.startswith('-')])
+
+
+@pytest.fixture
 def dispatch_command_mock(mocker):
     return mocker.patch('repomate.cli.dispatch_command', autospec=True)
 
 
-def test_happy_path(monkeypatch_sys_args, api_instance_mock, parse_args_mock,
-                    dispatch_command_mock, no_config_mock):
-    main.main()
+def test_happy_path(api_instance_mock, parse_args_mock, dispatch_command_mock,
+                    no_config_mock):
 
-    parse_args_mock.assert_called_once_with(monkeypatch_sys_args[1:])
+    sys_args = ['just some made up arguments'.split()]
+
+    main.main(sys_args)
+
+    parse_args_mock.assert_called_once_with(sys_args[1:])
     dispatch_command_mock.assert_called_once_with(PARSED_ARGS,
                                                   api_instance_mock)
 
 
 def test_does_not_raise_on_exception_in_parsing(
-        monkeypatch_sys_args, api_instance_mock, parse_args_mock,
-        dispatch_command_mock, no_config_mock):
+        api_instance_mock, parse_args_mock, dispatch_command_mock,
+        no_config_mock):
     """should just log, but not raise."""
     msg = "some nice error message"
     parse_args_mock.side_effect = raise_(Exception(msg))
+    sys_args = ['just some made up arguments'.split()]
 
-    main.main()
+    main.main(sys_args)
 
-    parse_args_mock.assert_called_once_with(monkeypatch_sys_args[1:])
+    parse_args_mock.assert_called_once_with(sys_args[1:])
     assert not dispatch_command_mock.called
 
 
 def test_does_not_raise_on_exception_in_handling_parsed_args(
-        monkeypatch_sys_args, api_instance_mock, parse_args_mock,
-        dispatch_command_mock):
+        api_instance_mock, parse_args_mock, dispatch_command_mock):
     """should just log, but not raise."""
     msg = "some nice error message"
     dispatch_command_mock.side_effect = raise_(Exception(msg))
+    sys_args = ['just some made up arguments'.split()]
 
-    main.main()
+    main.main(sys_args)
+
+
+def test_plugins_args(parse_args_mock, dispatch_command_mock,
+                      init_plugins_mock):
+    plugin_args = '-p javac -p pylint'.split()
+    sys_args = ['repomate', *plugin_args, *CLONE_ARGS]
+
+    main.main(sys_args)
+
+    init_plugins_mock.assert_called_once_with(['javac', 'pylint'])
+    parse_args_mock.assert_called_once_with(CLONE_ARGS)
+
+
+def test_no_plugins_arg(parse_args_mock, dispatch_command_mock,
+                        init_plugins_mock):
+    sys_args = ['repomate', '--no-plugins', *CLONE_ARGS]
+
+    main.main(sys_args)
+
+    assert not init_plugins_mock.called
+    parse_args_mock.assert_called_once_with(CLONE_ARGS)
+
+
+def test_plugin_with_subparser_name(parse_args_mock, dispatch_command_mock,
+                                    init_plugins_mock):
+    sys_args = ['repomate', '-p', 'javac', '-p', 'clone', *CLONE_ARGS]
+
+    main.main(sys_args)
+
+    init_plugins_mock.assert_called_once_with(['javac', 'clone'])
+    parse_args_mock.assert_called_once_with(CLONE_ARGS)
+
+
+def test_plug_arg_incompatible_with_no_plugins(dispatch_command_mock,
+                                               init_plugins_mock):
+    sys_args = ['repomate', '-p', 'javac', '--no-plugins', *CLONE_ARGS]
+
+    with pytest.raises(SystemExit):
+        main.main(sys_args)
+
+    assert not init_plugins_mock.called
+    assert not dispatch_command_mock.called
+
+
+def test_invalid_plug_options(dispatch_command_mock, init_plugins_mock):
+    # -f is not a valid option for plugins and should be bumped to the
+    # main parser
+    sys_args = ['repomate', '-p', 'javac', '-f', *CLONE_ARGS]
+
+    with pytest.raises(SystemExit):
+        main.main(sys_args)
+
+    init_plugins_mock.assert_called_once_with(['javac'])
+    assert not dispatch_command_mock.called
