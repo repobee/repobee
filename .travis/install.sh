7d14c487ae138ffeeace7d98f3696b63a0598bf6
Simon Larsén <slarse@kth.se>
2019-03-07 12:32:13 +0100

    Run maxOS builds with tox

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,9 +4,12 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     brew upgrade pyenv
     eval "$(pyenv init -)"
-    pyenv install "$PYTHON"
-    pyenv global "$PYTHON"
-    python -m pip install -e ".[TEST]"
+    pyenv install 3.5.4 --skip-existing
+    pyenv install 3.6.5 --skip-existing
+    pyenv install 3.7.0 --skip-existing
+    pyenv global 3.7.0
+    pip install pip --upgrade
+    pip install tox tox-pyenv
 else
     pip install -e ".[TEST]"
 fi
