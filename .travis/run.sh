0c1ddb6b280c608dd4389abe5a33f2df248f33ae
Simon Larsén <slarse@kth.se>
2019-03-07 14:56:35 +0100

    Add flake8 to CI and pre-commit #140

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+function run_flake8() {
+    pip install flake8
+    flake8 --ignore=WE503,E203
+}
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     eval "$(pyenv init -)"
     pyenv local 3.5.4 3.6.5 3.7.0
     pyenv global 3.7.0
+    run_flake8
     tox
 else
+    run_flake8
     pytest tests --cov=repomate --cov-branch
 fi
