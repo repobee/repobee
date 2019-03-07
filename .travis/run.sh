7e161759582bb8ce2f0e667b03bc245ec4722b37
Simon Larsén <slarse@kth.se>
2019-03-07 14:40:07 +0100

    Run macOS builds with tox (#141)

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,9 +2,9 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     eval "$(pyenv init -)"
-    pyenv global "$PYTHON"
-    python --version
-    python -m pytest tests --cov=repomate --cov-branch
+    pyenv local 3.5.4 3.6.5 3.7.0
+    pyenv global 3.7.0
+    tox
 else
     pytest tests --cov=repomate --cov-branch
 fi
