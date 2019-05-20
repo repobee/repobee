33abdeb167cee5fb558e80ebcefe9d5600c3f22b
Simon Larsén <slarse@kth.se>
2019-05-20 22:02:34 +0200

    Adapt gitlab_api to the API class

--- repobee/gitlab_api.py
+++ repobee/gitlab_api.py
@@ -19,6 +19,7 @@ import daiquiri
 import gitlab
 
 from repobee import exception
+from repobee import apimeta
 from repobee import tuples
 from repobee import util
 
@@ -72,8 +73,8 @@ def _try_api_request(ignore_statuses: Optional[Iterable[int]] = None):
         )
 
 
-class GitLabAPI:
-    User = collections.namedtuple("User", ("id", "login"))
+class GitLabAPI(apimeta.API):
+    _User = collections.namedtuple("_User", ("id", "login"))
 
     def __init__(self, base_url, token, org_name, user: str = None):
         if user:
@@ -81,15 +82,11 @@ class GitLabAPI:
         self._user = "oauth2"
         self._gitlab = gitlab.Gitlab(base_url, private_token=token)
         self._group_name = org_name
-        self._group = self.get_organization(self._group_name)
+        self._group = self._get_organization(self._group_name)
         self._token = token
         self._base_url = base_url
 
-    @property
-    def org(self):
-        return self._group
-
-    def get_teams_in(self, team_names: Iterable[str]):
+    def _get_teams_in(self, team_names: Iterable[str]):
         """Get all teams that match any team name in the team_names iterable.
 
         Args:
@@ -102,27 +99,6 @@ class GitLabAPI:
         with _try_api_request():
             return (
                 team for team in self.get_teams() if team.name in team_names
-            )
-
-    def delete_teams(self, team_names: Iterable[str]) -> None:
-        """Delete all teams that match any of the team names. Skip any team
-        name for which no team can be found.
-
-        Args:
-            team_names: A list of team names for teams to be deleted.
-        """
-        raise NotImplementedError()
-        deleted = set()  # only for logging
-        for team in self.get_teams_in(team_names):
-            team.delete()
-            deleted.add(team.name)
-            LOGGER.info("deleted team {}".format(team.name))
-
-        # only logging
-        missing = set(team_names) - deleted
-        if missing:
-            LOGGER.warning(
-                "could not find teams: {}".format(", ".join(missing))
             )
 
     def ensure_teams_and_members(
@@ -140,7 +116,7 @@ class GitLabAPI:
             A list of Team namedtuples of the teams corresponding to the keys
             of the member_lists mapping.
         """
-        teams = self.ensure_teams_exist(
+        teams = self._ensure_teams_exist(
             [str(team_name) for team_name in member_lists.keys()],
             permission=permission,
         )
@@ -148,14 +124,14 @@ class GitLabAPI:
         for team in [team for team in teams if member_lists[team.name]]:
             self._ensure_members_in_team(team, member_lists[team.name])
 
-        return list(self.get_teams_in(set(member_lists.keys())))
+        return list(self._get_teams_in(set(member_lists.keys())))
 
     def _ensure_members_in_team(self, team, members: Iterable[str]):
         """Add all of the users in ``members`` to a team. Skips any users that
         don't exist, or are already in the team.
         """
         existing_members = set(
-            member.login for member in self.get_members(team)
+            member.login for member in self._get_members(team)
         )
         missing_members = [
             member for member in members if member not in existing_members
@@ -173,30 +149,30 @@ class GitLabAPI:
                     ", ".join(existing_members), team.name
                 )
             )
-        self.add_to_team(missing_members, team)
+        self._add_to_team(missing_members, team)
 
-    def get_organization(self, org_name):
+    def _get_organization(self, org_name):
         return [
             g
             for g in self._gitlab.groups.list(search=org_name)
             if g.name == org_name
         ][0]
 
-    def get_members(self, group):
-        return [self.User(m.id, m.username) for m in group.members.list()]
+    def _get_members(self, group):
+        return [self._User(m.id, m.username) for m in group.members.list()]
 
     def get_teams(self):
         return self._gitlab.groups.list(id=self._group.id)
 
-    def add_to_team(self, members: Iterable[str], team):
+    def _add_to_team(self, members: Iterable[str], team):
         """Add members to a team.
 
         Args:
-            members: Users to add to the team.
+            members: _Users to add to the team.
             team: A Team.
         """
         with _try_api_request():
-            users = self.get_users(members)
+            users = self._get_users(members)
             for user in users:
                 team.members.create(
                     {
@@ -205,16 +181,16 @@ class GitLabAPI:
                     }
                 )
 
-    def get_users(self, usernames):
+    def _get_users(self, usernames):
         users = []
         for name in usernames:
             user = self._gitlab.users.list(username=name)
-            if not user:
-                LOGGER.warning("user {} could not be found".format(users))
+            # if not user:
+            # LOGGER.warning("user {} could not be found".format(user))
             users += user
         return users
 
-    def ensure_teams_exist(
+    def _ensure_teams_exist(
         self, team_names: Iterable[str], permission: str = "push"
     ) -> List[tuples.Team]:
         """Create any teams that do not yet exist.
@@ -250,7 +226,7 @@ class GitLabAPI:
                 teams.append(new_team)
         return teams
 
-    def create_repos(self, repo_infos: Iterable[tuples.Repo]):
+    def create_repos(self, repos: Iterable[tuples.Repo]):
         """Create repositories in the given organization according to the Repos.
         Repos that already exist are skipped.
 
@@ -261,37 +237,37 @@ class GitLabAPI:
             A list of urls to all repos corresponding to the Repos.
         """
         repo_urls = []
-        for info in repo_infos:
+        for repo in repos:
             created = False
             with _try_api_request(ignore_statuses=[400]):
                 repo_urls.append(
                     self._gitlab.projects.create(
                         {
-                            "name": info.name,
-                            "path": info.name,
-                            "description": info.description,
+                            "name": repo.name,
+                            "path": repo.name,
+                            "description": repo.description,
                             "visibility": "private"
-                            if info.private
+                            if repo.private
                             else "public",
-                            "namespace_id": info.team_id,
+                            "namespace_id": repo.team_id,
                         }
                     ).attributes["http_url_to_repo"]
                 )
                 LOGGER.info(
-                    "created {}/{}".format(self._group.name, info.name)
+                    "created {}/{}".format(self._group.name, repo.name)
                 )
                 created = True
 
             if not created:
                 # TODO optimize, this team get is redundant
-                team_name = self._gitlab.groups.get(info.team_id).path
+                team_name = self._gitlab.groups.get(repo.team_id).path
                 repo_urls.append(
                     self._gitlab.projects.get(
-                        "/".join([self._group.path, team_name, info.name])
+                        "/".join([self._group.path, team_name, repo.name])
                     ).attributes["http_url_to_repo"]
                 )
                 LOGGER.info(
-                    "{}/{} already exists".format(self._group.name, info.name)
+                    "{}/{} already exists".format(self._group.name, repo.name)
                 )
 
         return [self._insert_auth(url) for url in repo_urls]
@@ -354,143 +330,3 @@ class GitLabAPI:
             )
         auth = "{}:{}".format(self._user, self._token)
         return repo_url.replace("https://", "https://{}@".format(auth))
-
-    def get_issues(
-        self,
-        repo_names: Iterable[str],
-        state: str = "open",
-        title_regex: str = "",
-    ):
-        """Get all issues for the repos in repo_names an return a generator
-        that yields (repo_name, issue generator) tuples.
-
-        Args:
-            repo_names: An iterable of repo names.
-            state: Specifying the state of the issue ('open' or 'closed').
-            title_regex: If specified, only issues matching this regex are
-            returned. Defaults to the empty string (which matches anything).
-        Returns:
-            A generator that yields (repo_name, issue generator) tuples.
-        """
-        raise NotImplementedError()
-
-    def open_issue(
-        self, title: str, body: str, repo_names: Iterable[str]
-    ) -> None:
-        """Open the specified issue in all repos with the given names.
-
-        Args:
-            title: Title of the issue.
-            body: Body of the issue.
-            repo_names: Names of repos to open the issue in.
-        """
-        raise NotImplementedError()
-
-    def close_issue(self, title_regex: str, repo_names: Iterable[str]) -> None:
-        """Close any issues in the given repos whose titles match the title_regex.
-
-        Args:
-            title_regex: A regex to match against issue titles.
-            repo_names: Names of repositories to close issues in.
-        """
-        raise NotImplementedError()
-
-    def add_repos_to_review_teams(
-        self,
-        team_to_repos: Mapping[str, Iterable[str]],
-        issue: Optional[tuples.Issue],
-    ) -> None:
-        """Add repos to review teams. For each repo, an issue is opened, and
-        every user in the review team is assigned to it. If no issue is
-        specified, sensible defaults for title and body are used.
-
-        Args:
-            team_to_repos: A mapping from a team name to a sequence of repo
-                names.
-            issue: An an optional Issue tuple to override the default issue.
-        """
-        raise NotImplementedError()
-
-    def get_review_progress(
-        self, review_team_names, students, title_regex
-    ) -> Mapping[str, List[tuples.Review]]:
-        """Get the peer review progress for the specified review teams and
-        students. Only issues matching the title regex will be considered peer
-        review issues. If a reviewer has opened an issue in the assigned repo
-        with a title matching the regex, the review will be considered done.
-
-        Note that reviews only count if the student is in the review team for
-        that repo. Review teams must only have one associated repo, or the
-        repo is skipped. This could potentially be relaxed if there is reason
-        to, because it is not critical to the functionality of the algorithm.
-
-        Args:
-            review_team_names: Names of review teams.
-            students: The reviewing students (supposedly also the ones being
-                reviewed, but not necessarily)
-            title_regex: If an issue title matches this regex, the issue is
-                considered a potential peer review issue.
-        Returns:
-            a mapping (reviewer -> assigned_repos), where reviewer is a str and
-            assigned_repos is a :py:class:`~repobee.tuples.Review`.
-        """
-        raise NotImplementedError()
-
-    def add_repos_to_teams(self, team_to_repos: Mapping[str, Iterable[str]]):
-        """Add repos to teams and yield each (team, repo) combination _after_
-        the repo has been added to the team.
-
-        Args:
-            team_to_repos: A mapping from a team name to a sequence of repo
-                names.
-        Returns:
-            a generator yielding each (team, repo) tuple in turn.
-        """
-        raise NotImplementedError()
-
-    def _get_repos_by_name(self, repo_names: Iterable[str]):
-        """Get all repos that match any of the names in repo_names. Unmatched
-        names are ignored (in both directions).
-
-        Args:
-            repo_names: Names of repos to fetch.
-
-        Returns:
-            a generator of repo objects.
-        """
-        raise NotImplementedError()
-
-    @staticmethod
-    def verify_settings(
-        user: str,
-        org_name: str,
-        base_url: str,
-        token: str,
-        master_org_name: Optional[str] = None,
-    ):
-        """Verify the following:
-
-        .. code-block: markdown
-
-            1. Base url is correct (verify by fetching user).
-            2. The token has correct access privileges (verify by getting oauth
-               scopes)
-            3. Organization exists (verify by getting the org)
-                - If master_org_name is supplied, this is also checked to
-                  exist.
-            4. User is owner in organization (verify by getting
-                - If master_org_name is supplied, user is also checked to be an
-                  owner of it.
-            organization member list and checking roles)
-
-            Raises exceptions if something goes wrong.
-
-        Args:
-            user: The username to try to fetch.
-            org_name: Name of an organization.
-            base_url: A base url to a GitLab instance.
-            token: A secure OAUTH2 token.
-        Returns:
-            True if the connection is well formed.
-        """
-        raise NotImplementedError()
