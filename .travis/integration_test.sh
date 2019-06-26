c45fa915919005066fc6214a7e50c67c345f8b20
Simon Larsén <slarse@kth.se>
2019-06-26 18:03:37 +0200

    Try again

--- .travis/integration_test.sh
+++ .travis/integration_test.sh
@@ -2,12 +2,11 @@
 sudo docker build -t repobee:test -f Dockerfile.test .
 
 # setup virtualenv
-pip install pipenv
-pipenv install -e .[TEST]
+pip install -e .[TEST]
 
 # execute integration tests
 cd tests/integration_tests/
 sudo docker network create development
 ./startup.sh
-pipenv run sudo python -m pytest integration_tests.py
+pytest integration_tests.py
 cd -
