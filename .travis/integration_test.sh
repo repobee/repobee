183482157d5b41658b07e77950c65358f8014c70
Simon Larsén <slarse@kth.se>
2019-06-26 13:18:09 +0200

    Update integration test script

--- .travis/integration_test.sh
+++ .travis/integration_test.sh
@@ -1,5 +1,5 @@
 cd tests/integration_tests/
 sudo docker network create development
 ./startup.sh
-sudo docker run --name curl --rm --net development appropriate/curl -fsSl -k https://gitlab.integrationtest.local
+sudo docker run --name curl --rm --net development appropriate/curl -fsSl -k -H "Private-Token: $(cat token)" https://gitlab.integrationtest.local/api/v4/projects
 cd -
