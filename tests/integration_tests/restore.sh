9be0d6fdc0ff6ae88dfa452c39787bd0793d242b
Simon Larsén <slarse@kth.se>
2019-06-26 11:18:26 +0200

    Fix restore script

--- tests/integration_tests/restore.sh
+++ tests/integration_tests/restore.sh
@@ -1,8 +1,13 @@
-
+status="$(sudo docker inspect -f {{.State.Health.Status}} gitlab)"
 while [ "$status" != "healthy" ]; do 
+
+    if [ "$status" == "unhealthy" ]; then
+        exit 1
+    fi
+
     sleep 10
     status="$(sudo docker inspect -f {{.State.Health.Status}} gitlab)"
-    echo "GitLab is still starting ..."
+    echo "GitLab status: ${status} ..."
 done
 echo "Gitlab has started!"
 sudo docker exec -t gitlab chown git:git /var/opt/gitlab/backups/test_gitlab_backup.tar
