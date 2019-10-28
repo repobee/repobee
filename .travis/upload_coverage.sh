9d530022350f422b84a3a68254cd3f4cfed1f351
Simon Larsén <slarse@kth.se>
2019-10-28 11:04:15 +0100

    Run integration test coverage upload from correct directory

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -2,6 +2,7 @@
 
 if [[ $INTEGRATION_TEST == "true" ]];
 then
+    cd tests/integration_tests
     cat .coverage_files/report.txt
     rm -f .coverage_files/.coverage
     codecov --file .coverage_files/coverage.xml
