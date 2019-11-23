9613ec310dceb74ef2e5c2822f9c315c123b00d9
Simon Larsén <slarse@kth.se>
2019-11-23 17:01:27 +0100

    Run mac build only on Python 3.5.4

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-
     brew update --force
     brew upgrade pyenv
     eval "$(pyenv init -)"
     pyenv install 3.5.4 --skip-existing
-    pyenv install 3.6.5 --skip-existing
-    pyenv install 3.7.4 --skip-existing
-    pyenv global 3.7.4
+    pyenv global 3.5.4
     pip install pip --upgrade
-    pip install tox tox-pyenv
-else
-    pip install -e ".[TEST]"
 fi
+
+pip install -e ".[TEST]"
