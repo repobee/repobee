31b69812f70666688e086393b2d8b190e9b039ba
Simon Larsén <slarse@kth.se>
2019-11-24 17:08:37 +0100

    [misc] Run macOS build without caching and only on Python 3.5.4 (#387)

--- .travis/run.sh
+++ .travis/run.sh
@@ -12,11 +12,8 @@ fi
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     eval "$(pyenv init -)"
-    pyenv local 3.5.4 3.6.5 3.7.0
-    pyenv global 3.7.0
-    run_flake8
-    tox
-else
-    run_flake8
-    pytest tests/unit_tests --cov=_repobee --cov-branch
+    pyenv global 3.5.4
 fi
+
+run_flake8
+pytest tests --cov=_repobee --cov-branch
