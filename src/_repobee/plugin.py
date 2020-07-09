876ba2596bcaa887a59819d4e85a6c2ee97e9c3b
Simon Larsén <slarse@kth.se>
2020-07-09 12:32:46 +0200

    Add bandit to static analysis

--- src/_repobee/plugin.py
+++ src/_repobee/plugin.py
@@ -102,7 +102,11 @@ def _try_load_module_from_filepath(path: str) -> Optional[ModuleType]:
         The module if loaded successfully, or None if there was no module at
         the path.
     """
-    package_name = f"_{hashlib.sha1(path.encode(sys.getdefaultencoding()))}"
+    package_name = (
+        # The use of SHA1 here is not a security issue as it is only intended
+        # to come up with a (most likely) unique name for the package
+        f"_{hashlib.sha1(path.encode(sys.getdefaultencoding()))}"  # nosec
+    )
     module_name = pathlib.Path(path).stem
     qualname = f"{package_name}.{module_name}"
     spec = importlib.util.spec_from_file_location(qualname, path)
@@ -167,7 +171,7 @@ def unregister_all_plugins() -> None:
 
 
 def try_register_plugin(
-    plugin_module: ModuleType, *plugin_classes: List[type],
+    plugin_module: ModuleType, *plugin_classes: List[type]
 ) -> None:
     """Attempt to register a plugin module and then immediately unregister it.
 
