9cd75148176444884e9d2158db52e1d3a6105a2d
slarse <slarse@kth.se>
2020-07-10 19:55:25 +0200

    Try running with sudo

--- .travis/integration_test.sh
+++ .travis/integration_test.sh
@@ -3,5 +3,4 @@ docker build -t repobee:test -f Dockerfile.test .
 
 # execute integration tests
 cd tests/integration_tests/
-export REPOBEE_NO_VERIFY_SSL='true'
-pytest integration_tests.py -vv --capture=no
+sudo REPOBEE_NO_VERIFY_SSL=true pytest integration_tests.py -vv --capture=no
