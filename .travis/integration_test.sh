c4dc3ca4cdc742a340c60b184fe8c7869cd64625
slarse <slarse@kth.se>
2020-07-11 14:15:55 +0200

    Revert "Try running with sudo"
    This reverts commit 9cd75148176444884e9d2158db52e1d3a6105a2d.

--- .travis/integration_test.sh
+++ .travis/integration_test.sh
@@ -3,4 +3,5 @@ docker build -t repobee:test -f Dockerfile.test .
 
 # execute integration tests
 cd tests/integration_tests/
-sudo REPOBEE_NO_VERIFY_SSL=true pytest integration_tests.py -vv --capture=no
+export REPOBEE_NO_VERIFY_SSL='true'
+pytest integration_tests.py -vv --capture=no
