632ccc7752d2e5942f5f19bc41b97a5965d2f83c
slarse <slarse@kth.se>
2020-07-14 14:54:42 +0200

    Refactor suite

---
+++ tests/integration_tests/conftest.py
@@ -0,0 +1,217 @@
+import pathlib
+
+import _repobee.cli
+import gitlab
+import pytest
+import repobee_plug as plug
+
+import gitlabmanager
+from asserts import (
+    assert_repos_exist,
+    assert_on_groups,
+    assert_num_issues,
+    assert_issues_exist,
+)
+from const import (
+    VOLUME_DST,
+    COVERAGE_VOLUME_DST,
+    REPOBEE_GITLAB,
+    BASE_ARGS,
+    MASTER_ORG_ARG,
+    MASTER_REPOS_ARG,
+    STUDENTS_ARG,
+    STUDENT_TEAMS,
+    MASTER_REPO_NAMES,
+    LOCAL_BASE_URL,
+    TOKEN,
+    ORG_NAME,
+    STUDENT_TEAM_NAMES,
+    ACTUAL_USER,
+)
+from helpers import run_in_docker
+
+
+@pytest.fixture(autouse=True, scope="session")
+def setup_gitlab_instance():
+    """Perform first-time setup of the GitLab instance."""
+    gitlabmanager.setup()
+
+
+@pytest.fixture(autouse=True, scope="session")
+def teardown_gitlab_instance():
+    """Teardown the GitLab instance after all tests have finished."""
+    yield
+    gitlabmanager.teardown()
+
+
+@pytest.fixture(autouse=True)
+def restore():
+    """Run the script that restores the GitLab instance to its initial
+    state.
+    """
+    gitlabmanager.restore()
+
+
+@pytest.fixture
+def tmpdir_volume_arg(tmpdir):
+    """Create a temporary directory and return an argument string that
+    will mount a docker volume to it.
+    """
+    yield "-v {}:{}".format(str(tmpdir), VOLUME_DST)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def coverage_volume():
+    covdir = pathlib.Path(".").resolve() / ".coverage_files"
+    yield "-v {}:{}".format(str(covdir), COVERAGE_VOLUME_DST)
+    covfile = covdir / ".coverage"
+    assert covfile.is_file()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def generate_coverage_report(coverage_volume):
+    """Generate a coverage report after all tests have run."""
+    yield
+    # xml report for Codecov
+    run_in_docker(
+        "cd {} && coverage xml".format(COVERAGE_VOLUME_DST),
+        extra_args=[coverage_volume],
+    )
+    # txt report for manual inspection
+    run_in_docker(
+        "cd {} && coverage report > report.txt".format(COVERAGE_VOLUME_DST),
+        extra_args=[coverage_volume],
+    )
+
+
+@pytest.fixture(autouse=True)
+def handle_coverage_file(extra_args):
+    """Copy the coverage file back and forth."""
+    # copy the previous .coverage file into the workdir
+    run_in_docker(
+        "cp {}/.coverage .".format(COVERAGE_VOLUME_DST), extra_args=extra_args
+    )
+    yield
+    # copy the appended .coverage file into the coverage volume
+    run_in_docker(
+        "cp .coverage {}".format(COVERAGE_VOLUME_DST), extra_args=extra_args
+    )
+
+
+@pytest.fixture
+def extra_args(tmpdir_volume_arg, coverage_volume):
+    """Extra arguments to pass to run_in_docker when executing a test."""
+    return [tmpdir_volume_arg, coverage_volume]
+
+
+@pytest.fixture
+def with_student_repos(restore):
+    """Set up student repos before starting tests.
+
+    Note that explicitly including restore here is necessary to ensure that
+    it runs before this fixture.
+    """
+    command = " ".join(
+        [
+            REPOBEE_GITLAB,
+            _repobee.cli.mainparser.SETUP_PARSER,
+            *BASE_ARGS,
+            *MASTER_ORG_ARG,
+            *MASTER_REPOS_ARG,
+            *STUDENTS_ARG,
+        ]
+    )
+
+    result = run_in_docker(command)
+
+    # pre-test asserts
+    assert result.returncode == 0
+    assert_repos_exist(STUDENT_TEAMS, MASTER_REPO_NAMES)
+    assert_on_groups(STUDENT_TEAMS)
+
+
+@pytest.fixture
+def open_issues(with_student_repos):
+    """Open two issues in each student repo."""
+    task_issue = plug.Issue(
+        title="Task", body="The task is to do this, this and that"
+    )
+    correction_issue = plug.Issue(
+        title="Correction required", body="You need to fix this, this and that"
+    )
+    issues = [task_issue, correction_issue]
+    gl = gitlab.Gitlab(LOCAL_BASE_URL, private_token=TOKEN, ssl_verify=False)
+    target_group = gl.groups.list(search=ORG_NAME)[0]
+    projects = (
+        gl.projects.get(p.id)
+        for p in target_group.projects.list(include_subgroups=True, all=True)
+    )
+    for project in projects:
+        project.issues.create(
+            dict(title=task_issue.title, description=task_issue.body)
+        )
+        project.issues.create(
+            dict(
+                title=correction_issue.title, description=correction_issue.body
+            )
+        )
+
+    assert_num_issues(STUDENT_TEAM_NAMES, MASTER_REPO_NAMES, len(issues))
+    assert_issues_exist(STUDENT_TEAM_NAMES, MASTER_REPO_NAMES, task_issue)
+    assert_issues_exist(
+        STUDENT_TEAM_NAMES, MASTER_REPO_NAMES, correction_issue
+    )
+
+    return issues
+
+
+def expected_num_members_group_assertion(expected_num_members):
+    def group_assertion(expected, actual):
+        assert expected.name == actual.name
+        # +1 member for the group owner
+        assert len(actual.members.list(all=True)) == expected_num_members + 1
+        assert len(actual.projects.list(all=True)) == 1
+        project_name = actual.projects.list(all=True)[0].name
+        assert actual.name.startswith(project_name)
+        for member in actual.members.list(all=True):
+            if member.username == ACTUAL_USER:
+                continue
+            assert member.username not in project_name
+            assert member.access_level == gitlab.REPORTER_ACCESS
+
+    return group_assertion
+
+
+@pytest.fixture
+def with_reviews(with_student_repos):
+    master_repo_name = MASTER_REPO_NAMES[1]
+    expected_review_teams = [
+        plug.Team(
+            members=[],
+            name=plug.generate_review_team_name(
+                student_team_name, master_repo_name
+            ),
+        )
+        for student_team_name in STUDENT_TEAM_NAMES
+    ]
+    command = " ".join(
+        [
+            REPOBEE_GITLAB,
+            _repobee.cli.mainparser.ASSIGN_REVIEWS_PARSER,
+            *BASE_ARGS,
+            "--mn",
+            master_repo_name,
+            *STUDENTS_ARG,
+            "-n",
+            "1",
+        ]
+    )
+
+    result = run_in_docker(command)
+
+    assert result.returncode == 0
+    assert_on_groups(
+        expected_review_teams,
+        single_group_assertion=expected_num_members_group_assertion(1),
+    )
+    return (master_repo_name, expected_review_teams)
---
+++ tests/integration_tests/helpers.py
@@ -0,0 +1,118 @@
+import hashlib
+import os
+import pathlib
+import subprocess
+import sys
+import tempfile
+
+import _repobee.ext
+import gitlab
+
+from const import (
+    ORG_NAME,
+    LOCAL_BASE_URL,
+    TOKEN,
+    MASTER_ORG_NAME,
+    BASE_DOMAIN,
+    LOCAL_DOMAIN,
+)
+
+
+def api_instance(org_name=ORG_NAME):
+    """Return a valid instance of the GitLabAPI class."""
+    return _repobee.ext.gitlab.GitLabAPI(LOCAL_BASE_URL, TOKEN, org_name)
+
+
+def gitlab_and_groups():
+    """Return a valid gitlab instance, along with the master group and the
+    target group.
+    """
+    gl = gitlab.Gitlab(LOCAL_BASE_URL, private_token=TOKEN, ssl_verify=False)
+    master_group = gl.groups.list(search=MASTER_ORG_NAME)[0]
+    target_group = gl.groups.list(search=ORG_NAME)[0]
+    return gl, master_group, target_group
+
+
+def run_in_docker_with_coverage(command, extra_args=None):
+    assert extra_args, "extra volume args are required to run with coverage"
+    coverage_command = (
+        "coverage run --branch --append --source _repobee -m " + command
+    )
+    return run_in_docker(coverage_command, extra_args=extra_args)
+
+
+def run_in_docker(command, extra_args=None):
+    extra_args = " ".join(extra_args) if extra_args else ""
+    docker_command = (
+        "sudo docker run {} --net development --rm --name repobee "
+        "repobee:test /bin/sh -c '{}'"
+    ).format(extra_args, command)
+    proc = subprocess.run(
+        docker_command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    print(
+        proc.stdout.decode(sys.getdefaultencoding())
+    )  # for test output on failure
+    return proc
+
+
+def update_repo(repo_name, filename, text):
+    """Add a file with the given filename and text to the repo."""
+    gl = gitlab.Gitlab(LOCAL_BASE_URL, private_token=TOKEN, ssl_verify=False)
+    proj, *_ = [
+        p for p in gl.projects.list(search=repo_name) if p.name == repo_name
+    ]
+    env = os.environ.copy()
+    env["GIT_SSL_NO_VERIFY"] = "true"
+    env["GIT_AUTHOR_EMAIL"] = "slarse@kth.se"
+    env["GIT_AUTHOR_NAME"] = "Simon"
+    env["GIT_COMMITTER_EMAIL"] = env["GIT_AUTHOR_EMAIL"]
+    env["GIT_COMMITTER_NAME"] = env["GIT_AUTHOR_NAME"]
+    with tempfile.TemporaryDirectory() as tmpdir:
+        url_with_token = (
+            proj.web_url.replace(
+                "https://", "https://oauth2:{}@".format(TOKEN)
+            ).replace(BASE_DOMAIN, LOCAL_DOMAIN)
+            + ".git"
+        )
+        clone_proc = subprocess.run(
+            "git clone {}".format(url_with_token).split(), cwd=tmpdir, env=env
+        )
+        assert clone_proc.returncode == 0
+
+        repo_dir = pathlib.Path(tmpdir) / proj.name
+        new_file = repo_dir / filename
+        new_file.touch()
+        new_file.write_text(text)
+
+        add_proc = subprocess.run(
+            "git add {}".format(filename).split(), cwd=str(repo_dir), env=env
+        )
+        assert add_proc.returncode == 0
+
+        commit_proc = subprocess.run(
+            "git commit -am newfile".split(), cwd=str(repo_dir), env=env
+        )
+        assert commit_proc.returncode == 0
+
+        push_proc = subprocess.run(
+            "git push".split(), cwd=str(repo_dir), env=env
+        )
+        assert push_proc.returncode == 0
+
+    assert proj.files.get(filename, "master").decode().decode("utf8") == text
+
+
+def hash_directory(path):
+    shas = []
+    for dirpath, _, filenames in os.walk(str(path)):
+        if ".git" in dirpath:
+            continue
+        files = list(
+            pathlib.Path(dirpath) / filename for filename in filenames
+        )
+        shas += (hashlib.sha1(file.read_bytes()).digest() for file in files)
+    return hashlib.sha1(b"".join(sorted(shas))).digest()
