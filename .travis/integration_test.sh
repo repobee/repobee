0cdf76e66834c3234c032ddf9582a0f0ad90b464
slarse <slarse@kth.se>
2020-07-27 13:28:36 +0200

    Test only assign_one_review

--- .travis/integration_test.sh
+++ .travis/integration_test.sh
@@ -4,4 +4,4 @@ docker build -t repobee:test -f Dockerfile.test .
 # execute integration tests
 cd tests/integration_tests/
 export REPOBEE_NO_VERIFY_SSL='true'
-pytest integration_tests.py -vv
+pytest integration_tests.py -vv -k assign_one_review --capture=no
