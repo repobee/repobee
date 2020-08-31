607ed85ba90a4d7e08f4d46a32be88edaf9ebbac
Simon Larsén <slarse@slar.se>
2020-08-31 20:49:40 +0200

    Fix bug in migrate command

--- src/_repobee/command/repos.py
+++ src/_repobee/command/repos.py
@@ -359,7 +359,7 @@ def migrate_repos(
                 git.Push(
                     local_path=template_repo.path,
                     repo_url=api.insert_auth(template_repo.url),
-                    branch=git.active_branch(template_repo.repo_path),
+                    branch=git.active_branch(template_repo.path),
                 )
                 for template_repo in remote_templates
             ]
