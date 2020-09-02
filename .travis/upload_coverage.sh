a45ffb59d123b2ecd6afdf4a553fbb1c86ffe927
Simon Larsén <slarse@kth.se>
2020-09-02 22:28:35 +0200

    Fix coverage uploading for system test

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ $SYSTEM_TEST == "true" ]];
-then
-    cd system_tests
-    rm -f .coverage_files/.coverage
-    codecov --file .coverage_files/coverage.xml
-else
-    codecov
-fi
+cd system_tests
+rm -f .coverage_files/.coverage
+codecov --file .coverage_files/coverage.xml
