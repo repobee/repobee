9f8ee7d939aef669ffc63452bceb776803ecdcf6
Simon Larsén <slarse@slar.se>
2020-10-17 15:48:18 +0200

    [feat] Add ghclassroom plugin (#715)

---
+++ src/_repobee/ext/ghclassroom.py
@@ -0,0 +1,45 @@
+"""Interoperability plugin for using RepoBee with repositories created with
+GitHub Classroom.
+
+.. warning::
+
+    This plugin is in alpha and may change without notice.
+
+RepoBee's default naming scheme is ``<team_name>-<assignment_name>``, whereas
+GitHub Classroom's naming scheme is ``<assignment_name>-<team_name>``. This
+plugin changes RepoBee's naming scheme to conform to the latter, which allows
+RepoBee to be used with repositories created by GitHub Classroom.
+
+.. important::
+
+    The first time you use RepoBee with GitHub Classroom-created repositories,
+    you should run the ``repos setup`` command. This creates the teams that
+    RepoBee needs to find repositories, and allocates the GitHub
+    Classroom-created repositories to them.
+
+.. important::
+
+    You should not use this plugin to create repositories, and expect GitHub
+    Classroom to recognize them as student repositories. To be very clear,
+    Classroom *does not* recognize repositories created by RepoBee as student
+    repositories.
+"""
+from typing import Union
+
+import repobee_plug as plug
+
+
+PLUGIN_DESCRIPTION = """Allows interoperability with repositories created
+by GitHub Classroom by changing RepoBee's naming scheme to conform to that
+of Classroom. On first time use, you should run `repos setup` to allocate
+the teams that RepoBee needs to find repositories. (NOTE: This plugin is in
+alpha state and may change without notice)""".replace(
+    "\n", " "
+)
+
+
+@plug.repobee_hook
+def generate_repo_name(
+    team_name: Union[str, plug.StudentTeam, plug.Team], assignment_name: str
+) -> str:
+    return f"{assignment_name}-{team_name}"
