11e704d40408a214c3b7c4fa2ef9e5636fcde17f
Simon Larsén <slarse@slar.se>
2020-11-16 22:46:04 +0100

    Refactor creation of plugin command parsers

--- src/_repobee/cli/mainparser.py
+++ src/_repobee/cli/mainparser.py
@@ -10,7 +10,7 @@ import types
 import argparse
 import pathlib
 
-from typing import Union, Mapping
+from typing import Union, Mapping, List
 
 
 import repobee_plug as plug
@@ -562,112 +562,203 @@ def _add_extension_parsers(
         if isinstance(p, plug.cli.Command)
     ]
     for cmd in command_plugins:
+        _attach_command(
+            cmd,
+            base_parser,
+            base_student_parser,
+            template_org_parser,
+            repo_name_parser,
+            subparsers,
+            parsers_mapping,
+            parsed_config,
+        )
+
+
+def _attach_command(
+    cmd: plug.cli.Command,
+    base_parser: argparse.ArgumentParser,
+    base_student_parser: argparse.ArgumentParser,
+    template_org_parser: argparse.ArgumentParser,
+    repo_name_parser: argparse.ArgumentParser,
+    subparsers: argparse._SubParsersAction,
+    parsers_mapping: dict,
+    parsed_config: dict,
+) -> None:
+    category, action, is_category_action = _resolve_category_and_action(cmd)
+
+    parents = _compose_parent_parsers(
+        cmd,
+        base_parser,
+        base_student_parser,
+        template_org_parser,
+        repo_name_parser,
+    )
+
+    if category and category not in parsers_mapping and not is_category_action:
+        parsers_mapping[category] = _create_category_parser(
+            category, subparsers
+        )
+
+    assert action not in parsers_mapping
+
+    settings = cmd.__settings__
+    ext_parser = _create_action_parser(
+        cmd=cmd,
+        action=action,
+        is_category_action=is_category_action,
+        parsers_mapping=parsers_mapping,
+        subparsers=subparsers,
+        parents=parents,
+    )
+    cmd.attach_options(config=parsed_config, parser=ext_parser)
+
+    settings_dict = settings._asdict()
+    settings_dict.update(dict(action=action, category=category))
+    cmd.__settings__ = settings.__class__(**settings_dict)
+
+
+def _resolve_category_and_action(
+    cmd: plug.cli.Command,
+) -> categorization.Action:
+    settings = cmd.__settings__
+    category = (
+        settings.action.category
+        if isinstance(settings.action, categorization.Action)
+        else settings.category
+    )
+    action = settings.action or cmd.__class__.__name__.lower().replace(
+        "_", "-"
+    )
+
+    if isinstance(action, str):
         is_category_action = False
-        settings = cmd.__settings__
-        category = (
-            settings.action.category
-            if isinstance(settings.action, categorization.Action)
-            else settings.category
-        )
-        action = settings.action or cmd.__class__.__name__.lower().replace(
-            "_", "-"
-        )
-        if isinstance(action, str):
-            if not category:
-                is_category_action = True
-                category = plug.cli.category(
-                    name=action, action_names=[action]
-                )
-            action = (
+        if not category:
+            is_category_action = True
+            category = plug.cli.category(name=action, action_names=[action])
+        return (
+            category,
+            (
                 category[action]
                 if category and action in category
                 else categorization.Action(name=action, category=category)
-            )
-
-        parents = []
-        bp = plug.BaseParser
-        req_parsers = settings.base_parsers or []
-        if cmd.__requires_api__() or bp.BASE in req_parsers:
-            parents.append(base_parser)
-        if bp.STUDENTS in req_parsers:
-            parents.append(base_student_parser)
-        if bp.TEMPLATE_ORG in req_parsers:
-            parents.append(template_org_parser)
-
-        if bp.REPO_DISCOVERY in req_parsers:
-            parents.append(_REPO_DISCOVERY_PARSER)
-        elif bp.ASSIGNMENTS in req_parsers:
-            parents.append(repo_name_parser)
-
-        if (
-            category
-            and category not in parsers_mapping
-            and not is_category_action
-        ):
-            # new category
-            category_cmd = subparsers.add_parser(
-                name=category.name,
-                help=category.help,
-                description=category.description,
-            )
-            category_parsers = category_cmd.add_subparsers(dest=ACTION)
-            category_parsers.required = True
-            parsers_mapping[category] = category_parsers
-
-        assert action not in parsers_mapping
-
-        ext_parser = (parsers_mapping.get(category) or subparsers).add_parser(
-            action.name,
-            help=settings.help,
-            description=settings.description,
-            parents=parents,
-            formatter_class=OrderedFormatter,
+            ),
+            is_category_action,
         )
+    else:
+        return category, action, False
 
-        try:
-            _add_debug_args(ext_parser)
-        except argparse.ArgumentError:
-            pass
 
-        try:
-            # this will fail if we are adding arguments to an existing command
-            ext_parser.add_argument(
-                "--repobee-action",
-                action="store_const",
-                help=argparse.SUPPRESS,
-                const=action.name,
-                default=action.name,
-                dest="action",
-            )
-            # This is a little bit of a dirty trick. It allows us to easily
-            # find the associated extension command when parsing the arguments.
-            ext_parser.add_argument(
-                "--repobee-extension-command",
-                action="store_const",
-                help=argparse.SUPPRESS,
-                const=cmd,
-                default=cmd,
-                dest="_extension_command",
-            )
-        except argparse.ArgumentError:
-            pass
+def _compose_parent_parsers(
+    cmd: plug.cli.Command,
+    base_parser: argparse.ArgumentParser,
+    base_student_parser: argparse.ArgumentParser,
+    template_org_parser: argparse.ArgumentParser,
+    repo_name_parser: argparse.ArgumentParser,
+):
+    parents = []
+    bp = plug.BaseParser
+    req_parsers = cmd.__settings__.base_parsers or []
+    if cmd.__requires_api__() or bp.BASE in req_parsers:
+        parents.append(base_parser)
+    if bp.STUDENTS in req_parsers:
+        parents.append(base_student_parser)
+    if bp.TEMPLATE_ORG in req_parsers:
+        parents.append(template_org_parser)
 
-        if is_category_action:
-            # category is not specified, so it's a category-action
-            ext_parser.add_argument(
-                "--repobee-category",
-                action="store_const",
-                help=argparse.SUPPRESS,
-                const=category,
-                default=category,
-                dest="category",
-            )
+    if bp.REPO_DISCOVERY in req_parsers:
+        parents.append(_REPO_DISCOVERY_PARSER)
+    elif bp.ASSIGNMENTS in req_parsers:
+        parents.append(repo_name_parser)
 
-        cmd.attach_options(config=parsed_config, parser=ext_parser)
+    return parents
 
-        settings_dict = settings._asdict()
-        settings_dict.update(dict(action=action, category=category))
-        cmd.__settings__ = settings.__class__(**settings_dict)
+
+def _create_category_parser(
+    category: categorization.Category, subparsers: argparse._SubParsersAction
+) -> argparse.ArgumentParser:
+    category_cmd = subparsers.add_parser(
+        name=category.name,
+        help=category.help,
+        description=category.description,
+    )
+    category_parser = category_cmd.add_subparsers(dest=ACTION)
+    category_parser.required = True
+    return category_parser
+
+
+def _create_action_parser(
+    cmd: plug.cli.Command,
+    action: categorization.Action,
+    is_category_action: bool,
+    parsers_mapping: Mapping[categorization.Category, argparse.ArgumentParser],
+    subparsers: argparse.ArgumentParser,
+    parents: List[argparse.ArgumentParser],
+):
+    settings = cmd.__settings__
+    ext_parser = (
+        parsers_mapping.get(action.category) or subparsers
+    ).add_parser(
+        action.name,
+        help=settings.help,
+        description=settings.description,
+        parents=parents,
+        formatter_class=OrderedFormatter,
+    )
+
+    try:
+        _add_debug_args(ext_parser)
+    except argparse.ArgumentError:
+        pass
+
+    _add_metainfo_args(
+        ext_parser=ext_parser,
+        action=action,
+        cmd=cmd,
+        is_category_action=is_category_action,
+    )
+
+    return ext_parser
+
+
+def _add_metainfo_args(
+    ext_parser: argparse.ArgumentParser,
+    action: categorization.Action,
+    cmd: plug.cli.Command,
+    is_category_action: bool,
+) -> None:
+    try:
+        # this will fail if we are adding arguments to an existing command
+        ext_parser.add_argument(
+            "--repobee-action",
+            action="store_const",
+            help=argparse.SUPPRESS,
+            const=action.name,
+            default=action.name,
+            dest="action",
+        )
+        # This is a little bit of a dirty trick. It allows us to easily
+        # find the associated extension command when parsing the arguments.
+        ext_parser.add_argument(
+            "--repobee-extension-command",
+            action="store_const",
+            help=argparse.SUPPRESS,
+            const=cmd,
+            default=cmd,
+            dest="_extension_command",
+        )
+    except argparse.ArgumentError:
+        pass
+
+    if is_category_action:
+        # category is not specified, so it's a category-action
+        ext_parser.add_argument(
+            "--repobee-category",
+            action="store_const",
+            help=argparse.SUPPRESS,
+            const=action.category,
+            default=action.category,
+            dest="category",
+        )
 
 
 def _create_base_parsers(config_file):
