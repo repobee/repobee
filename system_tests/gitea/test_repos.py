42eb80f0ded4aa7267aa5d47763d5da7705f48ca
Simon Larsén <slarse@slar.se>
2021-01-05 23:04:13 +0100

    Simplify gitea system tests to run outside of docker

--- system_tests/gitea/test_repos.py
+++ system_tests/gitea/test_repos.py
@@ -1,36 +1,40 @@
+import shlex
 import re
 import itertools
 
 import git
 
+import repobee
+
 import repobee_plug as plug
 
-from repobee_testhelpers._internal.docker import run_in_docker_with_coverage
 from repobee_testhelpers._internal import templates as template_helpers
 
 import giteamanager
 
+from _repobee.ext import gitea
+
 
 class TestSetup:
-    def test_setup_once(self, extra_args, tmpdir):
+    def test_setup_once(self):
         """Test that running setup once results in the expected repos."""
 
         command = re.sub(
             r"\s+",
             " ",
             f"""
-repobee -p gitea repos setup --bu https://gitea.test.local:3000/api/v1
+repos setup --bu https://localhost:3000/api/v1
     --token {giteamanager.TEACHER_TOKEN}
     --user {giteamanager.TEACHER_USER}
     --org-name {giteamanager.TARGET_ORG_NAME}
     --template-org-name {giteamanager.TEMPLATE_ORG_NAME}
     --students {' '.join([t.members[0] for t in giteamanager.STUDENT_TEAMS])}
-    --assignments task-1 task-2 task-3
+    --assignments {' '.join(template_helpers.TEMPLATE_REPO_NAMES)}
     --tb
 """,
         )
 
-        run_in_docker_with_coverage(command, extra_args=extra_args)
+        repobee.run(shlex.split(command), plugins=[gitea])
 
         for team, template_name in itertools.product(
             giteamanager.STUDENT_TEAMS, template_helpers.TEMPLATE_REPO_NAMES
