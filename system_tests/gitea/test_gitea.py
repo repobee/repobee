34aa7593ec6008b50e81d7b36d4ecf7d5449b2b5
Simon Larsén <slarse@slar.se>
2021-01-05 09:31:53 +0100

    Add system tests for the GiteaAPI

---
+++ system_tests/gitea/test_gitea.py
@@ -0,0 +1,40 @@
+import pytest
+
+from _repobee.ext import gitea
+
+import giteamanager
+
+
+@pytest.fixture
+def gitea_api():
+    return gitea.GiteaAPI(
+        giteamanager.API_URL,
+        giteamanager.TEACHER_USER,
+        giteamanager.TEACHER_TOKEN,
+        giteamanager.TARGET_ORG_NAME,
+    )
+
+
+class TestCreateTeam:
+    """Tests for the create_team function."""
+
+    def test_create_non_existing_team(self, gitea_api):
+        team_name = "best-team"
+
+        gitea_api.create_team(team_name)
+
+        assert (
+            next(gitea_api.get_teams(team_names=[team_name])).name == team_name
+        )
+
+
+class TestGetTeams:
+    """Tests for the get_teams function."""
+
+    def test_get_owners_team(self, gitea_api):
+        owners_team_name = "Owners"
+        matches = list(gitea_api.get_teams(team_names=[owners_team_name]))
+
+        assert len(matches) == 1
+        assert matches[0].name == owners_team_name
+        assert matches[0].members == [giteamanager.TEACHER_USER]
