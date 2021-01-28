8627193dda560c91e67405e31fc7c6c7c61cfa1a
Simon Larsén <slarse@slar.se>
2021-01-28 21:37:31 +0100

    Fix remaining traces of salt terminology

--- src/_repobee/hash.py
+++ src/_repobee/hash.py
@@ -6,7 +6,7 @@ import hashlib
 
 def hash(obj: Any, max_hash_size: Optional[int] = None) -> str:
     """Return the hexdigest of a sha256 hash of the string representation of
-    the input object, salted with the giiven salt and truncated to the given
+    the input object, keyed with the giiven key and truncated to the given
     hash size.
 
     Args:
@@ -18,11 +18,9 @@ def hash(obj: Any, max_hash_size: Optional[int] = None) -> str:
     return hashlib.sha256(str(obj).encode("utf8")).hexdigest()[:max_hash_size]
 
 
-def salted_hash(
-    obj: Any, salt: Any, max_hash_size: Optional[int] = None
-) -> str:
+def keyed_hash(obj: Any, key: Any, max_hash_size: Optional[int] = None) -> str:
     """Return the hexdigest of a sha256 hash of the string representation of
-    the input object, salted with the giiven salt and truncated to the given
+    the input object, keyed with the giiven key and truncated to the given
     hash size.
 
     .. danger::
@@ -32,9 +30,9 @@ def salted_hash(
 
     Args:
         obj: Any object to calculate a hash for.
-        salt: A salt, typically a string.
+        key: A key, typically a string.
         max_hash_size: The maximum size of the returned hash.
     Returns:
         A hash as described.
     """
-    return hash(str(obj) + salt, max_hash_size=max_hash_size)
+    return hash(str(obj) + key, max_hash_size=max_hash_size)
