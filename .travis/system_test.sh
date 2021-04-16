6d5e845de7ef02ffc5fb8f5dd3b36c07520a0a74
Matthew Peveler <matt.peveler@gmail.com>
2021-04-15 20:43:09 -0400

    [test] remove unused travis-ci configuration (#889)

--- .travis/system_test.sh
+++
@@ -1,8 +0,0 @@
-# build RepoBee docker test container
-docker build -t repobee:test -f Dockerfile.test .
-
-# execute system tests
-cd system_tests
-export REPOBEE_NO_VERIFY_SSL='true'
-export PYTHONWARNINGS="ignore:Unverified HTTPS request"
-pytest test_gitlab_system.py -vv --showlocals
