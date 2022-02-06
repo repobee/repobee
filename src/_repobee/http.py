7c8600470965524a4e84aa5b7fc73eb17f231426
Simon Larsén <slarse@slar.se>
2022-02-06 11:31:15 +0100

    Fix pylint error

--- src/_repobee/http.py
+++ src/_repobee/http.py
@@ -13,6 +13,8 @@ _ORIGINAL_REQUESTS_METHODS = {
 
 DEFAULT_INTERNET_CONNECTION_CHECK_URL = "https://repobee.org"
 
+HTTP_OK = requests.codes.ok  # pylint: disable=E1101
+
 
 def rate_limit_modify_requests(
     base_url: str, rate_limit_in_seconds: float
@@ -75,8 +77,6 @@ def is_internet_connection_available(
         test_url: A URL to try to GET.
     """
     try:
-        return (
-            requests.get(test_url).status_code == requests.codes.ok
-        )  # pylint: disable=E1101
+        return requests.get(test_url).status_code == HTTP_OK
     except requests.exceptions.ConnectionError:
         return False
