9a355a04e6eb45db3976f34a5da6e59ca9a05cc0
Simon Larsén <slarse@slar.se>
2022-03-12 10:13:17 +0100

    Move inline disable to preceding line

--- tests/unit_tests/repobee_plug/test_plug_config.py
+++ tests/unit_tests/repobee_plug/test_plug_config.py
@@ -124,8 +124,7 @@ class TestConfig:
         # act/assert
         non_existing_key = "thiskeydoesntexist"
         with pytest.raises(KeyError) as exc_info:
-            child[parent_sec][
-                non_existing_key
-            ]  # pylint: disable=pointless-statement
+            # pylint: disable=pointless-statement
+            child[parent_sec][non_existing_key]
 
         assert non_existing_key in str(exc_info.value)
