b716fba3974707deab9fd24054e4fa67938f75eb
Simon Larsén <slarse@slar.se>
2022-08-31 22:24:16 +0200

    Implement retry-after handler

--- tests/unit_tests/repobee/test_http.py
+++ tests/unit_tests/repobee/test_http.py
@@ -74,6 +74,43 @@ class TestRateLimitModifyRequests:
         assert elapsed_time < rate_limit_in_seconds
 
 
+class TestRetryAfterHandler:
+    """Tests for the isntall_retry_after_handler function."""
+
+    @responses.activate(registry=responses.registries.OrderedRegistry)
+    @pytest.mark.parametrize("method_name", http.ALL_REQUEST_METHOD_NAMES)
+    def test_retries_after_header_time(self, method_name):
+        # arrange
+        url = urljoin(_ARBITRARY_BASE_URL, "/endpoint")
+        retry_after_in_seconds = .5
+
+        responses.add(
+            getattr(responses, method_name.upper()),
+            url,
+            status=403,
+            adding_headers={'Retry-After': str(retry_after_in_seconds)},
+        )
+        responses.add(
+            getattr(responses, method_name.upper()),
+            url,
+            status=200,
+        )
+
+        http.install_retry_after_handler()
+
+        request_function = getattr(requests, method_name)
+
+        # act
+        time_before = time.time()
+        response = request_function(url)
+        time_after = time.time()
+
+        # assert
+        elapsed_time = time_after - time_before
+        assert response.status_code == 200
+        assert elapsed_time > retry_after_in_seconds
+
+
 class TestRemoveRateLimits:
     """Tests for the remove_rate_limits function."""
 
